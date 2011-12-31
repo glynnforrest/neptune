2485416c9e7066ba0e4e6d10e853e8da475ee002
glynnforrest <me@glynnforrest.com>
2011-12-31 01:03:12 +0000

    ErrorHandler::init must now be called by user.

--- bootstrap.php
+++ bootstrap.php
@@ -2,11 +2,9 @@
 
 namespace neptune;
 
-use neptune\error\ErrorHandler;
 use neptune\core\Loader;
 
 require(__DIR__ . '/core/Loader.php');
 spl_autoload_register(__NAMESPACE__ . '\core\Loader::load');
 Loader::addNamespace('neptune', __DIR__ . '/');
-ErrorHandler::init();
 ?>
