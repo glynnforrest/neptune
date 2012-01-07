104f7bf7ad10cf491d5f07f2da88690ef1566bde
glynnforrest <me@glynnforrest.com>
2012-01-07 01:30:44 +0000

    Adding a simple events handling class.

---
+++ core/Events.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace neptune\core;
+
+/**
+ * Events
+ * @author Glynn Forrest me@glynnforrest.com
+ **/
+class Events {
+
+	protected static $instance;
+	protected $handlers = array();
+
+	protected function __construct() {
+	
+	}
+
+	public static function getInstance() {
+		if(!self::$instance) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	public function addHandler($name, $function) {
+		if(is_callable($function)) {
+			$this->handlers[$name] = $function;
+		}
+	}
+
+	public function send($name, $args = null) {
+		if(isset($this->handlers[$name])) {
+			$args = (array) $args;
+			return call_user_func_array($this->handlers[$name], $args);
+		}
+	}
+
+
+}
+?>
