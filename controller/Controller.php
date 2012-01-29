abd76f439143dde64d9e76173e9b66ae2f8d8ef7
glynnforrest <me@glynnforrest.com>
2012-01-29 20:16:30 +0000

    Binding Request and Response to Controller.
    This allows for easy access within controller methods
    by using $this->request or $this->response.

--- controller/Controller.php
+++ controller/Controller.php
@@ -3,18 +3,33 @@
 namespace neptune\controller;
 
 use neptune\exceptions\MethodNotFoundException;
+use neptune\security\SecurityFactory;
+use neptune\http\Request;
+use neptune\http\Response;
 
 /**
  * Base Controller
  */
 abstract class Controller {
 
+	protected $request;
+	protected $response;
+
 	public function __call($method, $args) {
 		throw new MethodNotFoundException('Function not found: ' . $method);
 	}
 
+	public function __construct() {
+		$this->request = Request::getInstance();
+		$this->response = Response::getInstance();
+	}
+
 	public function callHidden($method, $args) {
 		return call_user_func_array(array($this, $method), $args);
+	}
+
+	protected function security($name = null) {
+		return SecurityFactory::getSecurity($name);
 	}
 
 }
