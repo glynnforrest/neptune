be059b122f8e4aa71a8aacd8e90abdb7a4852d74
glynnforrest <me@glynnforrest.com>
2012-08-08 00:36:48 +0100

    Adding defaults to Console prompts.

--- console/Console.php
+++ console/Console.php
@@ -10,13 +10,16 @@ class Console {
 
 	protected static $instance;
 	protected $readline;
+	protected $prompt_suffix;
 	protected $fg_colour;
 	protected $bg_colour;
 	protected $error_fg_colour;
 	protected $error_bg_colour;
+	protected $default_options = array();
 
 	protected function __construct() {
 		$this->readline = extension_loaded('readline');
+		$this->prompt_suffix = PHP_EOL . '=> ';
 	}
 
 	public static function getInstance() {
@@ -42,13 +45,71 @@ class Console {
 		}
 	}
 
-	public function read($prompt = null) {
+	public function read($prompt = null, $default = null) {
+		$text = $this->addDefaultToPrompt($prompt, $default) .
+			$this->prompt_suffix;
 		if ($this->readline) {
-			return readline($prompt);
+			$input = readline($text);
+			$this->setDefaultOption($prompt, $input);
+			return $input;
 		}
-		$this->write($prefix, false);
-		return fgets(STDIN);
+		$this->write($text, false);
+		$input = fgets(STDIN);
+		$this->setDefaultOption($prompt, $input);
+		return $input;
 	}
+
+	public function readOptions(array $options, $prompt = null, $default = null) {
+		return $this->read($this->options($options, $prompt, $default));
+		//todo:check if supplied input matches options.
+	}
+
+	public function setPromptSuffix($string) {
+		$this->prompt_suffix = $string;
+	}
+
+
+	/** Create a string combining a prompt and an array of options.
+	 */
+	public function options(array $options, $prompt = null) {
+		$prompt = $prompt . ' [' . implode($options, ', ') . ']';
+	}
+
+	public function setDefaultOption($prompt, $option) {
+		$this->default_options[md5($prompt)] = $option;
+	}
+
+
+	/** Append a default to the end of a prompt.
+	 * If default is a string, use that string as a default.
+	 * If default is true, the last used answer for this prompt
+	 * will be used as a default.
+	 * If default is null, don't print a default.
+	 *
+	 */
+	protected function getDefaultOption($prompt, $default = null) {
+		if($default === true) {
+			if(isset($this->default_options[md5($prompt)])) {
+				return $this->default_options[md5($prompt)];
+			} else {
+				return null;
+			}
+		} elseif($default) {
+			return $default;
+		} else {
+			return null;
+		}
+	}
+
+	public function addDefaultToPrompt($prompt, $default = null) {
+		$default = $this->getDefaultOption($prompt, $default);
+		if($default) {
+			$prompt .= ' (Default: '. $default . ')';
+		}
+		return $prompt;
+	}
+
+
 
 }
 ?>
--- tests/console/ConsoleTest.php
+++ tests/console/ConsoleTest.php
@@ -26,7 +26,29 @@ class ConsoleTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals("Hello world", $out);
 	}
 
+	public function testNoDefault() {
+		$c = Console::getInstance();
+		$c->setDefaultOption('Pick a colour', 'black');
+		$this->assertEquals('Pick a colour', $c->addDefaultToPrompt('Pick a colour', null));
+	}
 
+	public function testNamedDefault() {
+		$c = Console::getInstance();
+		$c->setDefaultOption('Pick a colour', 'black');
+		$this->assertEquals('Pick a colour (Default: white)', $c->addDefaultToPrompt('Pick a colour', 'white'));
+	}
+
+	public function testLastDefault() {
+		$c = Console::getInstance();
+		$c->setDefaultOption('Pick a colour', 'black');
+		$this->assertEquals('Pick a colour (Default: black)', $c->addDefaultToPrompt('Pick a colour', true));
+	}
+
+	public function testOptions() {
+		$c = Console::getInstance();
+		$this->assertEquals('Pick a colour [0:black, 1:white]: ',
+							$c->options(array('black', 'white'), 'Pick a colour', null));
+	}
 
 }
 ?>
