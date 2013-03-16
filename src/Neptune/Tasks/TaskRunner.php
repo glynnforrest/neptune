1efa68966f3d154b2e55246009b5cd9a36b3c89b
glynnforrest <me@glynnforrest.com>
2013-03-16 18:08:18 +0000

    TaskRunner can now parse flags, locate Task classes and run them.

--- src/Neptune/Tasks/TaskRunner.php
+++ src/Neptune/Tasks/TaskRunner.php
@@ -2,6 +2,9 @@
 
 namespace Neptune\Tasks;
 
+use Neptune\Core\Config;
+use Neptune\Console\Console;
+use Neptune\Exceptions\ClassNotFoundException;
 use Neptune\Helpers\String;
 
 /**
@@ -11,6 +14,12 @@ use Neptune\Helpers\String;
 class TaskRunner {
 
 	protected static $instance;
+	protected $flags = array();
+	protected $flag_aliases = array(
+		'v' => 'verbose',
+		'i' => 'interactive',
+		'V' => 'version'
+	);
 
 	protected function __construct() {
 
@@ -28,6 +37,10 @@ class TaskRunner {
 	 * task name, the method to run, arguments and flags..
 	 */
 	public function parse($string) {
+		//if the command string is empty, default to something useful
+		if(trim($string) === '') {
+			$string = 'setup:help';
+		}
 		$pieces = explode(' ', $string);
 		//collect flags and strip them out so they won't be args
 		$flags = array();
@@ -37,7 +50,6 @@ class TaskRunner {
 				unset($pieces[$k]);
 			}
 		}
-		$this->processGlobalFlags($flags);
 		//collect args
 		$args = array();
 		for ($i = 1; $i < count($pieces); $i++) {
@@ -46,7 +58,7 @@ class TaskRunner {
 		//get the task name and method to run from the first argument
 		$task_pieces = explode(':', $pieces[0]);
 		$return = array();
-		$return['task'] = String::CamelCase($task_pieces[0]) . 'Task';
+		$return['task'] = $task_pieces[0];
 		if(isset($task_pieces[1])) {
 			$return['method'] = String::CamelCase($task_pieces[1], false);
 		} else {
@@ -57,8 +69,87 @@ class TaskRunner {
 		return $return;
 	}
 
-	public function processGlobalFlags(array $flags) {
+	public function run($command) {
+		$pieces = $this->parse($command);
+		try {
+			$class = $this->getTaskClass($pieces['task']);
+			$task = new $class();
+			$this->addFlags($pieces['flags']);
+			return call_user_func_array(array($task, $pieces['method']), $pieces['args']);
+		} catch (\Exception $e){
+			//this should probably be something else so we can track
+			//errors when run from a webpage
+			Console::getInstance()->error($e->getMessage());
+		}
+	}
 
+
+	/**
+	 * Get the full class name of a task.
+	 * This function will attempt to load a task in the following order:
+	 * - Application\Tasks\<Task>
+	 * - Neptune\Tasks\<Task>
+	 * - From every entry in the `task_paths` Config setting.
+	 **/
+	public function getTaskClass($task) {
+		$task =	String::CamelCase($task) . 'Task';
+		$candidates = array(
+			Config::get('namespace') . '\\Tasks\\'. $task,
+			'Neptune\\Tasks\\' . $task,
+		);
+		$more = array_map(function($namespace) use ($task) {
+				return $namespace . '\\' . $task;
+			}, Config::get('task.namespaces', array()));
+		$candidates = array_merge($candidates, $more);
+		foreach ($candidates as $class) {
+				if(class_exists($class)) {
+					return $class;
+				}
+		}
+		throw new ClassNotFoundException("$task not found.");
+	}
+
+
+
+	/**
+	 * Add flags to the current task.
+	 * This will not overwrite any previously set flags.
+	 **/
+	public function addFlags(array $flags) {
+		foreach ($flags as $flag) {
+			$flag = $this->lookupFlagName($flag);
+			if(!in_array($flag, $this->flags)) {
+				$this->flags[] = $flag;
+			}
+		}
+	}
+
+	/**
+	 * Get the full flag name of a given flag.
+	 * This will account for leading dashes and short forms.
+	 **/
+	protected function lookupFlagName($flag) {
+		$flag = trim($flag, '-');
+		if(isset($this->flag_aliases[$flag])) {
+			return $this->flag_aliases[$flag];
+		}
+		return strtolower($flag);
+	}
+
+	/**
+	 * Set the flags for the current task.
+	 * This will overwrite any previously set flags.
+	 **/
+	public function setFlags(array $flags) {
+		$this->flags = array();
+		$this->addFlags($flags);
+	}
+
+	/**
+	 * Get the flags in use for the current task.
+	 **/
+	public function getFlags() {
+		return $this->flags;
 	}
 
 
--- tests/Neptune/Tests/Tasks/TaskRunnerTest.php
+++ tests/Neptune/Tests/Tasks/TaskRunnerTest.php
@@ -2,6 +2,7 @@
 
 namespace Neptune\Tests\Tasks;
 
+use Neptune\Core\Config;
 use Neptune\Tasks\TaskRunner;
 
 require_once __DIR__ . '/../../../bootstrap.php';
@@ -12,6 +13,14 @@ require_once __DIR__ . '/../../../bootstrap.php';
  **/
 class TaskRunnerTest extends \PHPUnit_Framework_TestCase {
 
+	public function setUp() {
+		Config::create('testing');
+	}
+
+	public function tearDown() {
+		Config::unload();
+	}
+
 	public function testConstruct() {
 		$this->assertTrue(TaskRunner::getInstance() instanceof TaskRunner);
 	}
@@ -19,20 +28,20 @@ class TaskRunnerTest extends \PHPUnit_Framework_TestCase {
 	public function testParseSimple() {
 		$t = TaskRunner::getInstance();
 		$expected = array(
-			'task' => 'TestTask',
+			'task' => 'test',
 			'method' => 'run',
 			'args' => array(),
 			'flags' => array()
 		);
 		$this->assertEquals($expected, $t->parse('test'));
-		$expected['task'] = 'DoSomethingSpecialTask';
+		$expected['task'] = 'do-something-special';
 		$this->assertEquals($expected, $t->parse('do-something-special'));
 	}
 
 	public function testParseWithMethod() {
 		$t = TaskRunner::getInstance();
 		$expected = array(
-			'task' => 'SetupTask',
+			'task' => 'setup',
 			'method' => 'help',
 			'args' => array(),
 			'flags' => array()
@@ -45,7 +54,7 @@ class TaskRunnerTest extends \PHPUnit_Framework_TestCase {
 	public function testParseWithArguments() {
 		$t = TaskRunner::getInstance();
 		$expected = array(
-			'task' => 'SetupTask',
+			'task' => 'setup',
 			'method' => 'run',
 			'args' => array('one', 'two'),
 			'flags' => array()
@@ -56,7 +65,7 @@ class TaskRunnerTest extends \PHPUnit_Framework_TestCase {
 	public function testParseWithFlags() {
 		$t = TaskRunner::getInstance();
 		$expected = array(
-			'task' => 'BuildTask',
+			'task' => 'build',
 			'method' => 'run',
 			'args' => array(),
 			'flags' => array('--verbose'),
@@ -67,7 +76,7 @@ class TaskRunnerTest extends \PHPUnit_Framework_TestCase {
 	public function testParseWithMethodAndFlags() {
 		$t = TaskRunner::getInstance();
 		$expected = array(
-			'task' => 'BuildTask',
+			'task' => 'build',
 			'method' => 'setup',
 			'args' => array(),
 			'flags' => array('--verbose'),
@@ -78,12 +87,81 @@ class TaskRunnerTest extends \PHPUnit_Framework_TestCase {
 	public function testParseWithMethodArgsAndFlags() {
 		$t = TaskRunner::getInstance();
 		$expected = array(
-			'task' => 'CreateTask',
+			'task' => 'create',
 			'method' => 'model',
 			'args' => array('foo', 'bar', 'baz'),
 			'flags' => array('-v'),
 		);
 		$this->assertEquals($expected, $t->parse('create:model foo bar baz -v'));
+	}
+
+	public function testParseEmpty() {
+		$t = TaskRunner::getInstance();
+		$expected = array(
+			'task' => 'setup',
+			'method' => 'help',
+			'args' => array(),
+			'flags' => array(),
+		);
+		$this->assertEquals($expected, $t->parse(''));
+		$this->assertEquals($expected, $t->parse('   '));
+	}
+
+
+	public function testGetSetFlags() {
+		$t = TaskRunner::getInstance();
+		$t->setFlags(array('verbose'));
+		$this->assertEquals(array('verbose'), $t->getFlags());
+		$t->addFlags(array('verbose'));
+		$this->assertEquals(array('verbose'), $t->getFlags());
+		$t->setFlags(array('verbose', 'interactive'));
+		$this->assertEquals(array('verbose', 'interactive'), $t->getFlags());
+	}
+
+	public function testSetFlagsStripsDashes() {
+		$t = TaskRunner::getInstance();
+		$t->setFlags(array('--verbose'));
+		$this->assertEquals(array('verbose'), $t->getFlags());
+	}
+
+	public function testAddFlagsUsesAliases() {
+		$t = TaskRunner::getInstance();
+		$t->setFlags(array('v', 'i', 'V'));
+		$this->assertEquals(array('verbose', 'interactive', 'version'), $t->getFlags());
+	}
+
+	public function testAddFlagsIgnoresCase() {
+		$t = TaskRunner::getInstance();
+		$t->setFlags(array('--vErbose', '-interactiVE', 'VERSION'));
+		$this->assertEquals(array('verbose', 'interactive', 'version'), $t->getFlags());
+	}
+
+	public function testAddFlagsIgnoresDuplicates() {
+		$t = TaskRunner::getInstance();
+		$t->setFlags(array('--verbose', 'verbose'));
+		$this->assertEquals(array('verbose'), $t->getFlags());
+		$t->addFlags(array('-v'));
+		$this->assertEquals(array('verbose'), $t->getFlags());
+	}
+
+	public function testGetTaskClass() {
+		$t = TaskRunner::getInstance();
+		$this->assertEquals('Neptune\\Tasks\\SetupTask', $t->getTaskClass('setup'));
+		//pretend the Neptune\Tests namespace is our application and
+		//check it loads the DummyTask
+		Config::set('namespace', 'Neptune\\Tests');
+		$this->assertEquals('Neptune\\Tests\\Tasks\\DummyTask', $t->getTaskClass('dummy'));
+		//it should fail if a task namespace isn't set
+		Config::set('namespace', null);
+		$this->setExpectedException('Neptune\\Exceptions\\ClassNotFoundException');
+		$t->getTaskClass('dummy');
+		//now do the same with defining other task namespaces
+		Config::set('task.namespaces', array('Neptune\Tests\Tasks'));
+		$this->assertEquals('Neptune\\Tests\\Tasks\\DummyTask', $t->getTaskClass('dummy'));
+		//it should fail if a task namespace isn't set
+		Config::set('task.namespaces', null);
+		$this->setExpectedException('Neptune\\Exceptions\\ClassNotFoundException');
+		$t->getTaskClass('dummy');
 	}
 
 }
