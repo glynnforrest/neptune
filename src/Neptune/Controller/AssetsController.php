be20856e0d4210ac7fb450b6dd8ba3d7252074a0
glynnforrest <me@glynnforrest.com>
2013-07-02 11:14:03 +0100

    Adding comments and improving overall quality of AssetsController.

--- src/Neptune/Controller/AssetsController.php
+++ src/Neptune/Controller/AssetsController.php
@@ -22,13 +22,18 @@ class AssetsController extends Controller {
 		return true;
 	}
 
+	/**
+	 * Make an asset filter available to AssetsController.
+	 * $name is the name of the filter when used in configuration files.
+	 * $class_name is the fully qualified name to the filter class.
+	 */
 	public static function registerFilter($name, $class_name) {
 		self::$filters[$name] = $class_name;
 	}
 
 	protected function applyFilter(&$asset, $filter) {
 		if(!isset(self::$filters[$filter])) {
-			throw new Exception("Asset filter $filter has not been registered with AssetsController.");
+			throw new \Exception("Asset filter $filter has not been registered with AssetsController.");
 		}
 		$filter = new self::$filters[$filter];
 		$filter->filterAsset($asset);
@@ -36,16 +41,19 @@ class AssetsController extends Controller {
 	}
 
 
-	public function serveAsset($asset) {
-		$asset = urldecode($asset) . '.' . $this->request->format();
-		$asset = $this->processPrefix($asset);
+	public function serveAsset($asset_name) {
+		//decode url characters, we need &23 to #
+		$asset_name = urldecode($asset_name) . '.' . $this->request->format();
+		//use the correct config file for this asset by looking at the
+		//prefix. This is in the form prefix#asset
+		$asset_name = $this->processPrefix($asset_name);
 		try {
-			$a = new Asset($this->getAssetPath($asset));
-			foreach($this->getAssetFilters($asset) as $f) {
-				$this->applyFilter($a, $f);
+			$asset = new Asset($this->getAssetPath($asset_name));
+			foreach($this->getAssetFilters($asset_name) as $f) {
+				$this->applyFilter($asset, $f);
 			}
 			$this->response->setFormat($this->request->format());
-			return $a->getContent();
+			return $asset->getContent();
 		} catch (FileException $e) {
 			$this->response->setStatusCode('404');
 			return false;
@@ -67,13 +75,36 @@ class AssetsController extends Controller {
 		return Config::load()->get($this->current_prefix . 'assets.dir') . $filename;
 	}
 
+	/**
+	 * Get all filters that should be applied to $filename.
+	 *
+	 * This will look in 'assets.filters' for a list of regular
+	 * expressions to test $filename against. If $filename matches,
+	 * filter will be run on the asset.
+	 *
+	 * Example assets.filters:
+	 *
+	 *	'filters' => array(
+	 *	'`.*\.js$`' => 'minifyjs',
+	 *	'`.*\.css$`' => 'minifycss\filter'
+	 *	)
+	 */
 	public function getAssetFilters($filename) {
-		$filters = Config::load()->get($this->current_prefix . 'assets.filters');
+		//list of filters that have matched.
 		$matched = array();
-		if(is_array($filters) && !empty($filters)) {
-			foreach ($filters as $k => $v) {
-				if(preg_match($k, $filename)){
-					$matched[] = $v;
+		//grab the regexps to test against from the config file for this asset
+		$regexps = Config::load()->get($this->current_prefix . 'assets.filters');
+		if(is_array($regexps) && !empty($regexps)) {
+			foreach ($regexps as $regex => $filter_string) {
+				//check that $filename matches this regex
+				if(preg_match($regex, $filename)){
+					//$filename matches, but $filter_string can
+					//contain more than one filter, separated by
+					//|. Split $filter_string into seperate filters
+					//|and add to the matched list.
+					foreach (explode('|', $filter_string) as $filter) {
+						$matched[] = $filter;
+					}
 				}
 			}
 		}
@@ -81,4 +112,3 @@ class AssetsController extends Controller {
 	}
 
 }
-?>
