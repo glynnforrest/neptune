395b7bc54ac6f617317dbac1364114e246e93271
glynnforrest <me@glynnforrest.com>
2013-09-08 15:13:02 +0100

    Whitespace

--- src/Neptune/Core/Dispatcher.php
+++ src/Neptune/Core/Dispatcher.php
@@ -27,165 +27,165 @@ class Dispatcher {
         $this->request = Request::getInstance();
     }
 
-	public static function getInstance() {
-		if (!self::$instance) {
-			self::$instance = new self();
-		}
-		return self::$instance;
-	}
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 
-	/**
-	 * Create a new Route for the Dispatcher to handle with $url.
-	 */
-	public function route($url, $controller = null, $method = null, $args = null) {
-		//add a slash if the given url doesn't start with one
-		if(substr($url, 0, 1) !== '/' && $url !== '.*') {
-			$url = '/' . $url;
-		}
-		$route = clone $this->globals();
-		$route->url($url)->controller($controller)->method($method)->args($args);
-		$this->routes[$url] = $route;
-		return $this->routes[$url];
-	}
+    /**
+     * Create a new Route for the Dispatcher to handle with $url.
+     */
+    public function route($url, $controller = null, $method = null, $args = null) {
+        //add a slash if the given url doesn't start with one
+        if(substr($url, 0, 1) !== '/' && $url !== '.*') {
+            $url = '/' . $url;
+        }
+        $route = clone $this->globals();
+        $route->url($url)->controller($controller)->method($method)->args($args);
+        $this->routes[$url] = $route;
+        return $this->routes[$url];
+    }
 
-	public function globals() {
-		if(!$this->globals) {
-			$this->globals = new Route('.*');
-		}
-		return $this->globals;
-	}
+    public function globals() {
+        if(!$this->globals) {
+            $this->globals = new Route('.*');
+        }
+        return $this->globals;
+    }
 
-	/**
-	 * Serve assets with Neptune\Controller\AssetsController at the
-	 * config key assets.url.
-	 */
-	public function routeAssets() {
-		$url = Config::load('neptune')->getRequired('assets.url');
-		//add a slash if the given url doesn't start or end with one
-		if(substr($url, 0, 1) !== '/') {
-			$url = '/' . $url;
-		}
-		if(substr($url, -1, 1) !== '/') {
-			$url .= '/';
-		}
-		$url = $url . ':args';
-		$route = new Route($url);
-		$route->controller('Neptune\\Controller\\AssetsController')
-			  ->method('serveAsset')
-			  ->format('any')
-			  ->argsFormat(Route::ARGS_SINGLE);
-		$this->routes[$url] = $route;
-		return $this->routes[$url];
-	}
+    /**
+     * Serve assets with Neptune\Controller\AssetsController at the
+     * config key assets.url.
+     */
+    public function routeAssets() {
+        $url = Config::load('neptune')->getRequired('assets.url');
+        //add a slash if the given url doesn't start or end with one
+        if(substr($url, 0, 1) !== '/') {
+            $url = '/' . $url;
+        }
+        if(substr($url, -1, 1) !== '/') {
+            $url .= '/';
+        }
+        $url = $url . ':args';
+        $route = new Route($url);
+        $route->controller('Neptune\\Controller\\AssetsController')
+              ->method('serveAsset')
+              ->format('any')
+              ->argsFormat(Route::ARGS_SINGLE);
+        $this->routes[$url] = $route;
+        return $this->routes[$url];
+    }
 
-	public function catchAll($controller, $method ='index', $args = null) {
-		$url = '.*';
-		return $this->route($url, $controller, $method, $args)->format('any');
-	}
+    public function catchAll($controller, $method ='index', $args = null) {
+        $url = '.*';
+        return $this->route($url, $controller, $method, $args)->format('any');
+    }
 
-	public function clearRoutes() {
-		$this->routes = array();
-		return $this;
-	}
+    public function clearRoutes() {
+        $this->routes = array();
+        return $this;
+    }
 
-	public function clearGlobals() {
-		$this->globals = false;
-		return $this;
-	}
+    public function clearGlobals() {
+        $this->globals = false;
+        return $this;
+    }
 
-	public function goCached($source = null) {
-		if(!$source) {
-			$source = $this->request->path();
-		}
-		$key = 'Router' . $source . $this->request->method();
-		$cached = CacheFactory::getDriver()->get($key);
-		if($cached) {
-			if($this->runMethod($cached)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public function goCached($source = null) {
+        if(!$source) {
+            $source = $this->request->path();
+        }
+        $key = 'Router' . $source . $this->request->method();
+        $cached = CacheFactory::getDriver()->get($key);
+        if($cached) {
+            if($this->runMethod($cached)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public function go($source = null) {
-		if(!$source) {
-			$source = $this->request->path();
-		}
-		foreach($this->routes as $k => $v) {
-			if($v->test($source)) {
-				$actions = $v->getAction();
-				$this->matched_url = $k;
+    public function go($source = null) {
+        if(!$source) {
+            $source = $this->request->path();
+        }
+        foreach($this->routes as $k => $v) {
+            if($v->test($source)) {
+                $actions = $v->getAction();
+                $this->matched_url = $k;
                 $response = $this->runMethod($actions);
-				if($response) {
-					try {
-						$cm = CacheFactory::getDriver();
-						$key = 'Router' . $source . $this->request->method();
-						$cm->set($key, $actions);
-						$cm->set('Router.names', $this->names);
-					} catch	(\Exception $e) {
-					}
-					return $response;
-				}
-			}
-		}
-		return false;
-	}
+                if($response) {
+                    try {
+                        $cm = CacheFactory::getDriver();
+                        $key = 'Router' . $source . $this->request->method();
+                        $cm->set($key, $actions);
+                        $cm->set('Router.names', $this->names);
+                    } catch     (\Exception $e) {
+                    }
+                    return $response;
+                }
+            }
+        }
+        return false;
+    }
 
-	public static function missingArgsHandler($errno, $errstr, $errfile, $errline, $errcontext) {
-		$str = "Missing argument";
-		if ($str === substr($errstr, 0, strlen($str))) {
-			throw new ArgumentMissingException();
-		} else {
-			throw new NeptuneError($errno, $errstr, $errfile, $errline, $errcontext);
-		}
-	}
+    public static function missingArgsHandler($errno, $errstr, $errfile, $errline, $errcontext) {
+        $str = "Missing argument";
+        if ($str === substr($errstr, 0, strlen($str))) {
+            throw new ArgumentMissingException();
+        } else {
+            throw new NeptuneError($errno, $errstr, $errfile, $errline, $errcontext);
+        }
+    }
 
-	protected function runMethod($actions) {
-		if (class_exists($actions[0])) {
-			$c = new $actions[0]();
-			try {
-				set_error_handler('\Neptune\Core\Dispatcher::missingArgsHandler');
-				ob_start();
+    protected function runMethod($actions) {
+        if (class_exists($actions[0])) {
+            $c = new $actions[0]();
+            try {
+                set_error_handler('\Neptune\Core\Dispatcher::missingArgsHandler');
+                ob_start();
                 //$body is the return from the controller. $other is
                 //anything captured by output buffering, like echo and
                 //print.
-				$body = $c->_runMethod($actions[1], $actions[2]);
-				$this->other = ob_get_clean();
+                $body = $c->_runMethod($actions[1], $actions[2]);
+                $this->other = ob_get_clean();
                 //return false if there is no $body or $other. If
                 //there is no $body but $other exists, use that as the
                 //response.
-				if(!$body) {
+                if(!$body) {
                     if(!$this->other) {
                         return false;
                     }
                     restore_error_handler();
                     return $this->other;
-				}
-				restore_error_handler();
+                }
+                restore_error_handler();
                 return $body;
-			} catch (MethodNotFoundException $e) {
-				restore_error_handler();
-				return false;
-			} catch (ArgumentMissingException $e) {
-				restore_error_handler();
-				return false;
-			}
-			restore_error_handler();
-			return true;
-		}
-		return false;
-	}
+            } catch (MethodNotFoundException $e) {
+                restore_error_handler();
+                return false;
+            } catch (ArgumentMissingException $e) {
+                restore_error_handler();
+                return false;
+            }
+            restore_error_handler();
+            return true;
+        }
+        return false;
+    }
 
-	public function setRouteName($name, $url) {
-		$this->names[$name] = $url;
-	}
+    public function setRouteName($name, $url) {
+        $this->names[$name] = $url;
+    }
 
-	public function getRouteUrl($name) {
-		if(empty($this->names)) {
-			$this->names = CacheFactory::getDriver()->get('Router.names');
-		}
-		return isset($this->names[$name]) ? $this->names[$name] : null;
-	}
+    public function getRouteUrl($name) {
+        if(empty($this->names)) {
+            $this->names = CacheFactory::getDriver()->get('Router.names');
+        }
+        return isset($this->names[$name]) ? $this->names[$name] : null;
+    }
 
     public function getMatchedUrl() {
         return $this->matched_url;
--- tests/Neptune/Tests/Core/DispatcherTest.php
+++ tests/Neptune/Tests/Core/DispatcherTest.php
@@ -14,74 +14,74 @@ include __DIR__ . ('/../../../bootstrap.php');
  */
 class DispatcherTest extends \PHPUnit_Framework_TestCase {
 
-	public function setUp() {
-		Dispatcher::getInstance()->clearRoutes()->clearGlobals();
-	}
+    public function setUp() {
+        Dispatcher::getInstance()->clearRoutes()->clearGlobals();
+    }
 
-	public function testRouteReturnsRoute() {
-		$r = Dispatcher::getInstance()->route('/url');
-		$this->assertTrue($r instanceof Route);
-	}
+    public function testRouteReturnsRoute() {
+        $r = Dispatcher::getInstance()->route('/url');
+        $this->assertTrue($r instanceof Route);
+    }
 
-	public function testGlobalsReturnsRoute() {
-		$r = Dispatcher::getInstance()->globals();
-		$this->assertTrue($r instanceof Route);
-	}
+    public function testGlobalsReturnsRoute() {
+        $r = Dispatcher::getInstance()->globals();
+        $this->assertTrue($r instanceof Route);
+    }
 
-	public function testCatchAllReturnRoute() {
-		$r = Dispatcher::getInstance()->catchAll('foo');
-		$this->assertTrue($r instanceof Route);
-		$this->assertEquals('.*', $r->getUrl());
-	}
+    public function testCatchAllReturnRoute() {
+        $r = Dispatcher::getInstance()->catchAll('foo');
+        $this->assertTrue($r instanceof Route);
+        $this->assertEquals('.*', $r->getUrl());
+    }
 
-	public function testMissingSlash() {
-		$r = Dispatcher::getInstance()->route('test');
-		$this->assertEquals('/test', $r->getUrl());
-	}
+    public function testMissingSlash() {
+        $r = Dispatcher::getInstance()->route('test');
+        $this->assertEquals('/test', $r->getUrl());
+    }
 
-	public function testRouteInheritsGlobals() {
-		$d = Dispatcher::getInstance();
-		$d->globals()->transforms('controller', function($controller) {
-			return ucfirst($controller) . 'Controller';
-		});
-		$r = $d->route('/foo', 'foo', 'index');
-		$r->test('/foo');
-		$this->assertEquals(array('FooController', 'index', array()), $r->getAction());
-	}
+    public function testRouteInheritsGlobals() {
+        $d = Dispatcher::getInstance();
+        $d->globals()->transforms('controller', function($controller) {
+            return ucfirst($controller) . 'Controller';
+        });
+        $r = $d->route('/foo', 'foo', 'index');
+        $r->test('/foo');
+        $this->assertEquals(array('FooController', 'index', array()), $r->getAction());
+    }
 
-	public function testRouteAssets() {
-		$d = Dispatcher::getInstance();
+    public function testRouteAssets() {
+        $d = Dispatcher::getInstance();
         $c = Config::create('neptune');
         $c->set('assets.url', '/assets/');
-		$r = $d->routeAssets();
-		$this->assertEquals('/assets/:args', $r->getUrl());
-		$this->assertTrue($r->test('/assets/css/test'));
-		$this->assertEquals(
-			array(
-				'Neptune\Controller\AssetsController',
-				'serveAsset',
-				array('css/test')
-			),
-			$r->getAction());
+        $r = $d->routeAssets();
+        $this->assertEquals('/assets/:args', $r->getUrl());
+        $this->assertTrue($r->test('/assets/css/test'));
+        $this->assertEquals(
+            array(
+                'Neptune\Controller\AssetsController',
+                'serveAsset',
+                array('css/test')
+            ),
+            $r->getAction());
         Config::unload();
-	}
+    }
 
-	public function testRouteAssetsMissingSlashes() {
-		$d = Dispatcher::getInstance();
+    public function testRouteAssetsMissingSlashes() {
+        $d = Dispatcher::getInstance();
         $c = Config::create('neptune');
         $c->set('assets.url', '/assets/');
-		$r = $d->routeAssets();
-		$this->assertEquals('/assets/:args', $r->getUrl());
-		$this->assertTrue($r->test('/assets/lib/js/test'));
-		$this->assertEquals(
-			array(
-				'Neptune\Controller\AssetsController',
-				'serveAsset',
-				array('lib/js/test')
-			),
-			$r->getAction());
+        $r = $d->routeAssets();
+        $this->assertEquals('/assets/:args', $r->getUrl());
+        $this->assertTrue($r->test('/assets/lib/js/test'));
+        $this->assertEquals(
+            array(
+                'Neptune\Controller\AssetsController',
+                'serveAsset',
+                array('lib/js/test')
+            ),
+            $r->getAction());
         Config::unload();
-	}
+    }
 
     public function testGoReturnsControllerResponse() {
         $d = Dispatcher::getInstance();
