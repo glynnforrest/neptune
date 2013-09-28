d321ad5912cbc1b887f02f574486f6e451821c34
glynnforrest <me@glynnforrest.com>
2013-09-28 17:55:47 +0100

    Refactoring ConsoleTest.

--- tests/Neptune/Tests/Console/ConsoleTest.php
+++ tests/Neptune/Tests/Console/ConsoleTest.php
@@ -12,62 +12,60 @@ require_once __DIR__ . '/../../../bootstrap.php';
  */
 class ConsoleTest extends \PHPUnit_Framework_TestCase {
 
+	protected $c;
+
+	public function setUp() {
+		$this->c = Console::getInstance();
+	}
+
 	public function testWrite() {
-		$c = Console::getInstance();
 		ob_start();
-		$c->write('Hello world');
+		$this->c->write('Hello world');
 		$out = ob_get_clean();
 		$this->assertEquals("Hello world\n", $out);
 	}
 
 	public function testWriteNoNewLine() {
-		$c = Console::getInstance();
 		ob_start();
-		$c->write('Hello world', false);
+		$this->c->write('Hello world', false);
 		$out = ob_get_clean();
 		$this->assertEquals("Hello world", $out);
 	}
 
 	public function testNoDefault() {
-		$c = Console::getInstance();
-		$c->setDefaultOption('Pick a colour', 'black');
-		$this->assertEquals('Pick a colour', $c->addDefaultToPrompt('Pick a colour', null));
-		$this->assertEquals('Pick a colour', $c->addDefaultToPrompt('Pick a colour', ""));
+		$this->c->setDefaultOption('Pick a colour', 'black');
+		$this->assertEquals('Pick a colour', $this->c->addDefaultToPrompt('Pick a colour', null));
+		$this->assertEquals('Pick a colour', $this->c->addDefaultToPrompt('Pick a colour', ""));
 	}
 
 	public function testNamedDefault() {
-		$c = Console::getInstance();
-		$c->setDefaultOption('Pick a colour', 'black');
-		$this->assertEquals('Pick a colour (Default: white)', $c->addDefaultToPrompt('Pick a colour', 'white'));
+		$this->c->setDefaultOption('Pick a colour', 'black');
+		$this->assertEquals('Pick a colour (Default: white)', $this->c->addDefaultToPrompt('Pick a colour', 'white'));
 	}
 
 	public function testDefaultWorksWithZero() {
-		$c = Console::getInstance();
-		$this->assertEquals('Enter a number (Default: 0)', $c->addDefaultToPrompt('Enter a number', 0));
-		$this->assertEquals('Enter a number (Default: 0)', $c->addDefaultToPrompt('Enter a number', '0'));
+		$this->assertEquals('Enter a number (Default: 0)', $this->c->addDefaultToPrompt('Enter a number', 0));
+		$this->assertEquals('Enter a number (Default: 0)', $this->c->addDefaultToPrompt('Enter a number', '0'));
 	}
 
 	public function testLastDefault() {
-		$c = Console::getInstance();
-		$c->setDefaultOption('Pick a colour', 'black');
-		$this->assertEquals('Pick a colour (Default: black)', $c->addDefaultToPrompt('Pick a colour', true));
+		$this->c->setDefaultOption('Pick a colour', 'black');
+		$this->assertEquals('Pick a colour (Default: black)', $this->c->addDefaultToPrompt('Pick a colour', true));
 	}
 
 	public function testOptions() {
-		$c = Console::getInstance();
 		$expected = 'Pick a colour [0:black, 1:white] ';
-		$actual = $c->options(array('black', 'white'), 'Pick a colour');
+		$actual = $this->c->options(array('black', 'white'), 'Pick a colour');
 		$this->assertEquals($expected, $actual);
 		$expected ='Pick a colour [0:black, 1:white, 2:red, 3:yellow, 4:green] ';
-		$actual = $c->options(array('black', 'white', 'red', 'yellow', 'green'),
+		$actual = $this->c->options(array('black', 'white', 'red', 'yellow', 'green'),
 							  'Pick a colour');
 		$this->assertEquals($expected, $actual);
 	}
 
 	public function testOptionsEmptyArray() {
-		$c = Console::getInstance();
 		$this->setExpectedException('\\Neptune\\Console\\ConsoleException');
-		$c->options(array());
+		$this->c->options(array());
 	}
 
 }
