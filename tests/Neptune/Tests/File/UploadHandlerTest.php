2415906770aeb7b3f347d059ed7fceaa1082d532
glynnforrest <me@glynnforrest.com>
2013-10-07 00:44:58 +0100

    Removing old UploadHandler test case.
    New test to be implemented.

--- tests/Neptune/Tests/File/UploadHandlerTest.php
+++ tests/Neptune/Tests/File/UploadHandlerTest.php
@@ -19,7 +19,6 @@ class UploadHandlerTest extends \PHPUnit_Framework_TestCase {
 	protected $files_index = 'file';
 	protected $object;
 
-
 	public function setUp() {
 		Temping::getInstance()->create($this->filename);
 		$_FILES = array();
@@ -30,50 +29,17 @@ class UploadHandlerTest extends \PHPUnit_Framework_TestCase {
 			'error' => 0,
 			'size' => 0
 		);
+		$_SERVER['REQUEST_METHOD'] = 'POST';
 		$this->object = new UploadHandler($this->files_index);
 	}
 
 	public function tearDown() {
 		Temping::getInstance()->reset();
+		$_SERVER['REQUEST_METHOD'] = null;
 	}
 
 	public function testConstruct() {
 		$this->assertTrue($this->object instanceof UploadHandler);
-	}
-
-	public function testGetFilename() {
-		$this->assertEquals('file.txt', $this->object->getFilename());
-	}
-
-	public function testSetFilename() {
-		$this->object->setFilename('renamed.txt');
-		$this->assertEquals('renamed.txt', $this->object->getFilename());
-	}
-
-	public function testScrambleFilename() {
-		$this->object->scrambleFilename();
-		$this->assertNotEquals('file.txt', $this->object->getFilename());
-		//assert the random string is 16 characters long by default (.txt = 20).
-		$this->assertTrue(strlen($this->object->getFilename()) === 20);
-		//assert the random string is alphanumeric by default.
-		$this->assertRegexp('`^\w+\.txt$`', $this->object->getFilename());
-		$this->assertTrue(preg_match('`^\W+`', $this->object->getFilename()) === 0);
-	}
-
-	public function testScrambleOptionsLength() {
-		$this->object->setScrambleOptions(8);
-		$this->object->scrambleFilename();
-		$this->assertNotEquals('file.txt', $this->object->getFilename());
-		$this->assertTrue(strlen($this->object->getFilename()) === 12);
-	}
-
-	public function testScrambleOptions() {
-		$this->object->setScrambleOptions(6, String::HEX);
-		$this->object->scrambleFilename();
-		$this->assertNotEquals('file.txt', $this->object->getFilename());
-		$this->assertTrue(strlen($this->object->getFilename()) === 10);
-		$this->assertRegexp('`^[0-9A-F]+\.txt$`', $this->object->getFilename());
-		$this->assertTrue(preg_match('`[G-Z]+`', $this->object->getFilename()) === 0);
 	}
 
 }
