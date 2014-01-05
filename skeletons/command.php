ced5fb80b0ce5274daa6a7336f9e7ab7163b652e
Glynn Forrest <me@glynnforrest.com>
2014-01-05 17:17:44 +0000

    Adding create:command.

---
+++ skeletons/command.php
@@ -0,0 +1,17 @@
+
+namespace <?=$this->namespace;?>\Command;
+
+use Neptune\Command\Command;
+use Neptune\Console\Console;
+
+<?=$this->class_info;?>
+class <?=$this->class_name;?> extends Command {
+
+	protected $name = '<?=$this->name?>';
+	protected $description = '<?=$this->description;?>';
+
+	public function go(Console $console) {
+		$console->writeln('Hello from <?=$this->name;?>');
+	}
+
+}
---
+++ src/Neptune/Command/CreateCommandCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Neptune\Command;
+
+use Neptune\Command\CreateCommand;
+use Neptune\View\Skeleton;
+
+use Stringy\StaticStringy as S;
+
+/**
+ * CreateCommandCommand
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class CreateCommandCommand extends CreateCommand {
+
+	protected $name = 'create:command';
+	protected $description = 'Create a new command and test';
+	protected $prompt = 'Command name: ';
+	protected $default = 'say:hello';
+
+	protected function getTargetPath($name) {
+		return 'Command/' . $this->createClassName($name) . '.php';
+	}
+
+	protected function createClassName($name) {
+		return S::UpperCamelize(str_replace(':', '_', $name)) . 'Command';
+	}
+
+	protected function getSkeleton($name) {
+		$skeleton = Skeleton::loadAbsolute($this->getSkeletonPath('command'));
+		$skeleton->class_name = $this->createClassName($name);
+		$skeleton->name = $name;
+		$skeleton->description = 'Description for ' . $name;
+		return $skeleton;
+	}
+
+}
