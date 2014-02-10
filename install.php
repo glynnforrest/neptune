c4c96700d50e215105a1e7496642b213429c3afb
Glynn Forrest <me@glynnforrest.com>
2014-02-10 11:30:11 +0000

    Don't use dev version of neptune in install.php.

--- install.php
+++ install.php
@@ -1,35 +1,34 @@
 <?php
 
-function write($text) {
-	echo $text . PHP_EOL;
+function write($text)
+{
+    echo $text . PHP_EOL;
 }
 
 write('Installing neptune...');
 
 $project_dir = getcwd();
 
-if(!is_writable($project_dir)) {
-	write('Unable write to ' . $project_dir);
-	write('Make sure the path is writeable and permissions are set correctly.');
-	exit(1);
+if (!is_writable($project_dir)) {
+    write('Unable write to ' . $project_dir);
+    write('Make sure the path is writeable and permissions are set correctly.');
+    exit(1);
 }
 
 $json = '{
-	"require": {
-		"glynnforrest/neptune":"dev-master"
-	}
+    "require": {
+        "glynnforrest/neptune":"0.3.*"
+    }
 }
 ';
 
 try {
-	$file = new \SplFileObject('composer.json', 'w');
-	$file->fwrite($json);
+    $file = new \SplFileObject('composer.json', 'w');
+    $file->fwrite($json);
 } catch (\Exception $e) {
-	write($e->getMessage());
-	exit(1);
+    write($e->getMessage());
+    exit(1);
 }
-
-
 
 passthru('composer install');
 write('Neptune successfully downloaded.');
