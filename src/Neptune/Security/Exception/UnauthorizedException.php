cc53a222463f32be5471429236559ef5a9bec90a
Glynn Forrest <me@glynnforrest.com>
2014-02-14 16:40:59 +0000

    Adding UnauthorizedException.

---
+++ src/Neptune/Security/Exception/UnauthorizedException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Neptune\Security\Exception;
+
+/**
+ * UnauthorizedException is thrown when authentication is required.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class UnauthorizedException extends SecurityException
+{
+
+    public function __construct(SecurityDriverInterface $driver, $message = 'Authentication required', \Exception $previous = null)
+    {
+        parent::__construct($driver, $message, 401, $previous);
+    }
+
+}
