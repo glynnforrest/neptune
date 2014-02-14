9c4840379262a969dfac18d53abbb9f1dae5054c
Glynn Forrest <me@glynnforrest.com>
2014-02-14 16:09:14 +0000

    Adding base SecurityException class.
    All of these exceptions should have access to the security driver the
    Exception originated from.

---
+++ src/Neptune/Security/Exception/SecurityException.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Neptune\Security\Exception;
+
+use Neptune\Security\Driver\SecurityDriverInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * SecurityException
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SecurityException extends \Exception
+{
+
+    protected $driver;
+
+    public function __construct(SecurityDriverInterface $driver, $message = '', $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->driver = $driver;
+    }
+
+    public function setSecurityDriver(SecurityDriverInterface $driver)
+    {
+        $this->driver = $driver;
+    }
+
+    public function getSecurityDriver()
+    {
+        return $this->driver;
+    }
+
+}
