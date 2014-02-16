5ff51c365f510a5982165e41ac099aa80a0700fa
Glynn Forrest <me@glynnforrest.com>
2014-02-16 21:12:10 +0000

    The comeback of SecurityFactory - this time good

---
+++ src/Neptune/Security/SecurityFactory.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Neptune\Security;
+
+use Neptune\Security\Driver\SecurityDriverInterface;
+use Neptune\Security\Driver\PassDriver;
+use Neptune\Security\Driver\FailDriver;
+
+use Neptune\Core\AbstractFactory;
+use Neptune\Exceptions\ConfigKeyException;
+use Neptune\Exceptions\DriverNotFoundException;
+
+/**
+ * SecurityFactory
+ * @author Glynn Forrest <me@glynnforrest.com>
+ */
+class SecurityFactory extends AbstractFactory
+{
+
+    protected function create($name = null)
+    {
+        if (!$name) {
+            $names = array_keys($this->config->get('security.drivers', array()));
+            if (empty($names)) {
+                throw new ConfigKeyException(
+                    'Security drivers configuration array is empty');
+            }
+            $name = $names[0];
+        }
+        //if the entry in the config is a string, load it as a service
+        $maybe_service = $this->config->getRequired("security.drivers.$name");
+        if (is_string($maybe_service)) {
+            //check the service implements security interface first
+            $service = $this->neptune[$maybe_service];
+            if ($service instanceof SecurityDriverInterface) {
+                return $service;
+            }
+            throw new DriverNotFoundException(sprintf(
+                "Security driver '%s' requested service '%s' which does not implement Neptune\Security\Driver\SecurityDriverInterface",
+                $name,
+                $maybe_service));
+        }
+
+        $driver = $this->config->getRequired("security.drivers.$name.driver");
+
+        $method = 'create' . ucfirst($driver) . 'Driver';
+        if (method_exists($this, $method)) {
+            $this->instances[$name] = $this->$method($name);
+
+            return $this->instances[$name];
+        }
+        throw new DriverNotFoundException("Security driver not implemented: $driver");
+    }
+
+    public function createPassDriver($name)
+    {
+        return new PassDriver();
+    }
+
+    public function createFailDriver($name)
+    {
+        return new FailDriver();
+    }
+
+}
---
+++ tests/Neptune/Tests/Security/SecurityFactoryTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Neptune\Tests\Security;
+
+use Neptune\Security\SecurityFactory;
+use Neptune\Core\Config;
+
+use Temping\Temping;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+/**
+ * SecurityFactoryTest
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SecurityFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    protected $neptune;
+    protected $config;
+    protected $factory;
+
+    public function setUp()
+    {
+        $this->config = Config::create('neptune');
+
+        $this->config->set('security.drivers.driver1', array(
+            'driver' => 'pass',
+        ));
+
+        $this->config->set('security.drivers.driver2', array(
+            'driver' => 'fail',
+        ));
+
+        $this->neptune = $this->getMockBuilder('\Neptune\Core\Neptune')
+                              ->disableOriginalConstructor()
+                              ->getMock();
+        $this->factory = new SecurityFactory($this->config, $this->neptune);
+    }
+
+    public function tearDown()
+    {
+        Config::unload();
+    }
+
+    public function testGetDefaultDriver()
+    {
+        $driver = $this->factory->get();
+        $this->assertInstanceOf('\Neptune\Security\Driver\PassDriver', $driver);
+        $this->assertSame($driver, $this->factory->get());
+    }
+
+    public function testGetPassDriver()
+    {
+        $driver = $this->factory->get('driver1');
+        $this->assertInstanceOf('\Neptune\Security\Driver\PassDriver', $driver);
+        $this->assertSame($driver, $this->factory->get('driver1'));
+    }
+
+    public function testGetFailDriver()
+    {
+        $driver = $this->factory->get('driver2');
+        $this->assertInstanceOf('\Neptune\Security\Driver\FailDriver', $driver);
+        $this->assertSame($driver, $this->factory->get('driver2'));
+    }
+
+    public function testGetNoDefinition()
+    {
+        $this->setExpectedException('\\Neptune\\Exceptions\\ConfigKeyException');
+        $this->factory->get('wrong');
+    }
+
+    public function testGetDefaultNoConfig()
+    {
+        $this->setExpectedException('\\Neptune\\Exceptions\\ConfigKeyException');
+        $factory = new SecurityFactory(Config::create('empty'), $this->neptune);
+        $factory->get();
+    }
+
+    public function testGetNoDriver()
+    {
+        $this->setExpectedException('\\Neptune\\Exceptions\\ConfigKeyException');
+        $this->config->set('security.wrong', array(
+            //no driver
+        ));
+        $this->factory->get('wrong');
+    }
+
+    public function testGetUndefinedDriver()
+    {
+        $this->setExpectedException('\\Neptune\\Exceptions\\DriverNotFoundException');
+        $this->config->set('security.drivers.unknown', array('driver' => 'unicorn'));
+        $this->factory->get('unknown');
+    }
+
+    public function testGetDriverAsAService()
+    {
+        $driver = $this->getMock('\\Neptune\\Security\\Driver\\SecurityDriverInterface');
+        $this->neptune->expects($this->once())
+                      ->method('offsetGet')
+                      ->with('service.foo')
+                      ->will($this->returnValue($driver));
+        $this->config->set('security.drivers.foo', 'service.foo');
+        $this->assertSame($driver, $this->factory->get('foo'));
+    }
+
+    public function testGetDriverAsAServiceThrowsException()
+    {
+        $driver = new \stdClass();
+        $this->neptune->expects($this->once())
+                      ->method('offsetGet')
+                      ->with('service.foo')
+                      ->will($this->returnValue($driver));
+        $this->config->set('security.drivers.foo', 'service.foo');
+        $msg = "Security driver 'foo' requested service 'service.foo' which does not implement Neptune\Security\Driver\SecurityDriverInterface";
+        $this->setExpectedException('\Neptune\Exceptions\DriverNotFoundException');
+        $this->factory->get('foo');
+    }
+
+}
