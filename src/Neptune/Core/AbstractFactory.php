bc425340d48237bce17096a3ab072290b6985528
Glynn Forrest <me@glynnforrest.com>
2014-02-16 20:57:13 +0000

    Creating an AbstractFactory.

---
+++ src/Neptune/Core/AbstractFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Neptune\Core;
+
+/**
+ * AbstractFactory
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+abstract class AbstractFactory
+{
+
+    protected $config;
+    protected $neptune;
+    protected $instances = array();
+
+    public function __construct(Config $config, Neptune $neptune)
+    {
+        $this->config = $config;
+        $this->neptune = $neptune;
+    }
+
+    /**
+     * Get the instance called $name in the config instance. If $name
+     * is not specified, the first instance will be returned.
+     *
+     * @param string $name The name of the instance to get
+     */
+    public function get($name = null)
+    {
+        if (!$name) {
+            if (!empty($this->instances)) {
+                reset($this->instances);
+
+                return current($this->instances);
+            } else {
+                return $this->create();
+            }
+        }
+        if (array_key_exists($name, $this->instances)) {
+            return $this->instances[$name];
+        }
+
+        return $this->create($name);
+    }
+
+    abstract protected function create($name = null);
+
+}
