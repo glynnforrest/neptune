b8143deb4a133db9cda48dd7c187b5dfcc1c2de6
Glynn Forrest <me@glynnforrest.com>
2014-02-24 18:37:12 +0000

    Adding SecurityService.

---
+++ src/Neptune/Service/SecurityService.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Neptune\Service;
+
+use Neptune\Core\Neptune;
+use Neptune\Core\Config;
+use Neptune\Security\Firewall;
+use Neptune\Security\SecurityFactory;
+use Neptune\Security\Csrf\CsrfManager;
+use Neptune\EventListener\FirewallListener;
+use Neptune\EventListener\CsrfListener;
+use Neptune\EventListener\SecurityExceptionListener;
+
+use Symfony\Component\HttpFoundation\RequestMatcher;
+
+/**
+ * SecurityService
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SecurityService implements ServiceInterface
+{
+
+    protected $config;
+
+    public function __construct(Config $config = null)
+    {
+        $this->config = $config;
+    }
+
+    public function register(Neptune $neptune)
+    {
+        //if no config was supplied, grab the default
+        if (!$this->config) {
+            $config = $neptune['config'];
+        }
+
+        $neptune['security'] = function () use ($neptune, $config) {
+            return new SecurityFactory($config, $neptune);
+        };
+
+        //register firewalls if defined
+        if ($config->get('security.firewalls', false)) {
+            $this->registerFirewall($neptune, $config);
+        }
+
+        //register csrf by default
+        if ($config->get('security.csrf', true)) {
+            $this->registerCsrf($neptune, $config);
+        }
+
+        $neptune['security.resolver'] = function () {
+            return new SecurityExceptionListener();
+            //add resolvers automatically
+        };
+
+    }
+
+    protected function registerFirewall(Neptune $neptune, Config $config)
+    {
+        $neptune['security.firewall'] = function () use ($neptune, $config) {
+            $listener = new FirewallListener();
+            foreach ($config->get('security.firewalls', array()) as $name => $firewall) {
+                $listener->add($this->createFirewall($neptune, $config, $name));
+            }
+
+            return $listener;
+        };
+    }
+
+    protected function createFirewall(Neptune $neptune, Config $config, $name)
+    {
+        $driver_key = $config->get("security.firewalls.$name.driver");
+        $driver = $neptune['security']->get($driver_key);
+        $firewall = new Firewall($name, $driver);
+
+        // register rules
+        $rules = $config->get("security.firewalls.$name.rules", array());
+        foreach ($rules as $rule => $permission) {
+            $firewall->addRule(new RequestMatcher($rule), $permission);
+        }
+
+        // register exemptions
+        $exemptions = $config->get("security.firewalls.$name.exemptions", array());
+        foreach ($exemptions as $exemption => $permission) {
+            $firewall->addExemption(new RequestMatcher($exemption), $permission);
+        }
+
+        return $firewall;
+    }
+
+    protected function registerCsrf(Neptune $neptune, Config $config)
+    {
+        $neptune['security.csrf'] = function () use ($neptune, $config) {
+            //get the session and form tokens
+            $session_token = $config->get('security.csrf.session_token', 'security.csrf.token');
+            $form_token = $config->get('security.csrf.form_token', '_token');
+
+            $manager = new CsrfManager($neptune['session'], $session_token);
+
+            return new CsrfListener($manager, $form_token);
+        };
+    }
+
+    public function boot(Neptune $neptune)
+    {
+        $neptune['dispatcher']->addSubscriber($neptune['security.resolver']);
+
+        //add the firewall if set up
+        if ($neptune->offsetExists('security.firewall')) {
+            $neptune['dispatcher']->addSubscriber($neptune['security.firewall']);
+        }
+
+        //add csrf if set up
+        if ($neptune->offsetExists('security.csrf')) {
+            $neptune['dispatcher']->addSubscriber($neptune['security.csrf']);
+        }
+    }
+
+}
