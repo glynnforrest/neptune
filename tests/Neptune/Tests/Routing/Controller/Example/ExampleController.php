8a7b5b2fc0e41cc9e056e78b31fc319704fc9197
Glynn Forrest <me@glynnforrest.com>
2014-03-03 00:31:46 +0000

    Committing forgotten test fixture.

---
+++ tests/Neptune/Tests/Routing/Controller/Example/ExampleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Neptune\Tests\Routing\Controller\Example;
+
+use Neptune\Controller\Controller;
+
+/**
+ * ExampleController
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class ExampleController extends Controller {
+
+}
