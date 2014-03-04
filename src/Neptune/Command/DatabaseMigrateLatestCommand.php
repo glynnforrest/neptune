68335be297dfd53b5857c70fbc6c5740ecc3393c
Glynn Forrest <me@glynnforrest.com>
2014-03-04 10:54:01 +0000

    Adding database:migrate:latest command.

---
+++ src/Neptune/Command/DatabaseMigrateLatestCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Neptune\Command;
+
+use Neptune\Console\Console;
+use Neptune\Console\ConsoleLogger;
+use Neptune\Core\Config;
+use Neptune\Database\DatabaseFactory;
+use Neptune\Database\Drivers\ConsoleDriver;
+use Neptune\Database\Migration\MigrationRunner;
+
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * DatabaseMigrateLatestCommand
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class DatabaseMigrateLatestCommand extends Command
+{
+
+    protected $name = 'database:migrate:latest';
+    protected $description = 'Migrate to the latest version of the database schema';
+
+    protected function configure()
+    {
+        parent::configure();
+        $this->addOption(
+                 'module',
+                 'm',
+                 InputOption::VALUE_REQUIRED,
+                 'The module containing the migrations',
+                 $this->getDefaultModule()
+             );
+    }
+
+    public function go(Console $console)
+    {
+        $driver = new ConsoleDriver(DatabaseFactory::getDriver(), $this->output);
+        $runner = new MigrationRunner($driver, new ConsoleLogger($this->output));
+
+        $module = $this->input->getOption('module');
+        $path = $this->getModuleDirectory($module) . 'Migrations/';
+        $namespace = $this->getModuleNamespace($module) . '\\Migrations\\';
+        $runner->migrateLatest($path, $namespace);
+    }
+
+}
