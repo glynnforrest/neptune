b99cdb5133833aef753b8c7ae0628c0409afb659
Glynn Forrest <me@glynnforrest.com>
2014-03-04 10:53:04 +0000

    Adding CreateMigrationCommand.

---
+++ src/Neptune/Command/CreateMigrationCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Neptune\Command;
+
+use Neptune\View\Skeleton;
+
+/**
+ * CreateMigrationCommand
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class CreateMigrationCommand extends CreateCommand
+{
+
+    protected $name = 'create:migration';
+    protected $description = 'Create a new migration';
+
+    protected function getTargetPath($name)
+    {
+        return 'Migrations/' . $name . '.php';
+    }
+
+    protected function getResourceName()
+    {
+        return 'Migration' . date('YmdHis') ;
+    }
+
+    protected function getSkeleton($name)
+    {
+        $skeleton = Skeleton::loadAbsolute($this->getSkeletonPath('migration'));
+        $skeleton->class_name = $name;
+
+        return $skeleton;
+    }
+
+}
