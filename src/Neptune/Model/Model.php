045fb379b28778c3d10cfea42793971c30b51f14
Glynn Forrest <me@glynnforrest.com>
2014-03-26 14:34:28 +0000

    Removing Model logic - candidate for future removal.

--- src/Neptune/Model/Model.php
+++ src/Neptune/Model/Model.php
@@ -10,24 +10,4 @@ use Neptune\Cache\Cacheable;
  **/
 class Model extends Cacheable {
 
-	protected static $models = array();
-	protected $database;
-
-	protected function __construct() {
-	}
-
-	public static function getInstance($database = false) {
-		$class = get_called_class();
-		$model_name = $database ? $class . '.' . $database : $class;
-		if (!isset(self::$models[$model_name])) {
-			self::$models[$model_name] = new $class();
-			self::$models[$model_name]->setDatabase($database);
-		}
-		return self::$models[$model_name];
-	}
-
-	public function setDatabase($database) {
-		$this->database = $database;
-	}
-
 }
