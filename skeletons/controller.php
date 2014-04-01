ac867665181c2cd4975d2af5e92f468ede4a3ba7
Glynn Forrest <me@glynnforrest.com>
2014-04-01 15:13:25 +0000

    Updating controller skeleton.

--- skeletons/controller.php
+++ skeletons/controller.php
@@ -4,13 +4,15 @@ namespace <?=$this->namespace;?>\Controller;
 use Neptune\Controller\Controller;
 use Neptune\View\View;
 
+use Symfony\Component\HttpFoundation\Request;
+
 <?=$this->class_info;?>
 class <?=$this->controller_name;?> extends Controller
 {
 
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return View::load('index');
+        return 'Hello from <?$this->class_info;?>';
     }
 
 }
