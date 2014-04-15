8a6190f2aefa7c3316bd54f2ebfae387299792f1
Glynn Forrest <me@glynnforrest.com>
2014-04-15 14:35:44 +0000

    Deleting old routing example.

--- tests/Neptune/Tests/Routing/etc/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-return function(\Neptune\Routing\Router $router, $module, $prefix) {
-    $router->globals()->controller("::$module.controller.bar");
-    $router->route("$prefix/login")->method("{$module}_module_method");
-    $router->name('secret')->route("$prefix/secret")->method('secretArea');
-};
