c9f4ad20b1c58bb0730a7e0aa17e48a532e17394
Glynn Forrest <me@glynnforrest.com>
2014-04-15 17:15:01 +0000

    Updating command skeleton to follow psr-2.

--- skeletons/command.php
+++ skeletons/command.php
@@ -5,13 +5,15 @@ use Neptune\Command\Command;
 use Neptune\Console\Console;
 
 <?=$this->class_info;?>
-class <?=$this->class_name;?> extends Command {
+class <?=$this->class_name;?> extends Command
+{
 
-	protected $name = '<?=$this->name?>';
-	protected $description = '<?=$this->description;?>';
+    protected $name = '<?=$this->name?>';
+    protected $description = '<?=$this->description;?>';
 
-	public function go(Console $console) {
-		$console->writeln('Hello from <?=$this->name;?>');
-	}
+    public function go(Console $console)
+    {
+        $console->writeln('Hello from <?=$this->name;?>');
+    }
 
 }
