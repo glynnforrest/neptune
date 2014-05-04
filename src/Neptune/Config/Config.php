e574b416a336dff5a780bee7f581e7ea13dd17b0
Glynn Forrest <me@glynnforrest.com>
2014-05-04 09:23:10 +0100

    Running Config and test through php-cs-fixer.

--- src/Neptune/Config/Config.php
+++ src/Neptune/Config/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Neptune\Core;
+namespace Neptune\Config;
 
 use Neptune\Exceptions\ConfigKeyException;
 use Neptune\Exceptions\ConfigFileException;
@@ -11,145 +11,157 @@ use Crutches\DotArray;
  * Config
  * @author Glynn Forrest <me@glynnforrest.com>
  */
-class Config {
-
-	protected $name;
-	protected $filename;
-	protected $modified = false;
-	protected $dot_array;
+class Config
+{
+    protected $name;
+    protected $filename;
+    protected $modified = false;
+    protected $dot_array;
     protected $original;
 
-	public function __construct($name, $filename = null) {
-		if($filename) {
-			if(!file_exists($filename)) {
-				throw new ConfigFileException(
-					'Configuration file ' . $filename . ' not found');
-			}
-			$values = include $filename;
-			if (!is_array($values)) {
-				throw new ConfigFileException(
-					'Configuration file ' . $filename . ' does not return a php array');
-			}
-			$this->filename = $filename;
-		} else {
-			$values = array();
-		}
-		$this->name = $name;
-		$this->dot_array = new DotArray($values);
+    public function __construct($name, $filename = null)
+    {
+        if ($filename) {
+            if (!file_exists($filename)) {
+                throw new ConfigFileException(
+                    'Configuration file ' . $filename . ' not found');
+            }
+            $values = include $filename;
+            if (!is_array($values)) {
+                throw new ConfigFileException(
+                    'Configuration file ' . $filename . ' does not return a php array');
+            }
+            $this->filename = $filename;
+        } else {
+            $values = array();
+        }
+        $this->name = $name;
+        $this->dot_array = new DotArray($values);
         $this->original = new DotArray($values);
-		return true;
-	}
 
-	/**
-	 * Get a configuration value that matches $key.
-	 * $key uses the dot array syntax: parent.child.child
-	 * If the key matches an array the whole array will be returned.
-	 * If no key is specified the entire configuration array will be
-	 * returned.
-	 * $default will be returned (null unless specified) if the key is
-	 * not found.
-	 */
-	public function get($key = null, $default = null) {
-		return $this->dot_array->get($key, $default);
-	}
+        return true;
+    }
 
-	/**
-	 * Get the first value from an array of configuration values that
-	 * matches $key.
-	 * $default will be returned (null unless specified) if the key is
-	 * not found or does not contain an array.
-	 */
-	public function getFirst($key = null, $default = null) {
-		return $this->dot_array->getFirst($key, $default);
-	}
+    /**
+     * Get a configuration value that matches $key.
+     * $key uses the dot array syntax: parent.child.child
+     * If the key matches an array the whole array will be returned.
+     * If no key is specified the entire configuration array will be
+     * returned.
+     * $default will be returned (null unless specified) if the key is
+     * not found.
+     */
+    public function get($key = null, $default = null)
+    {
+        return $this->dot_array->get($key, $default);
+    }
 
-	/**
-	 * Get a configuration value that matches $key in the same way as
-	 * get(), but a ConfigKeyException will be thrown if
-	 * the key is not found.
-	 */
-	public function getRequired($key) {
-		$value = $this->get($key);
-		if (null !== $value) {
-			return $value;
-		}
-		throw new ConfigKeyException("Required value not found in Config instance '$this->name': $key");
-	}
+    /**
+     * Get the first value from an array of configuration values that
+     * matches $key.
+     * $default will be returned (null unless specified) if the key is
+     * not found or does not contain an array.
+     */
+    public function getFirst($key = null, $default = null)
+    {
+        return $this->dot_array->getFirst($key, $default);
+    }
 
-	/**
-	 * Get a directory path from the configuration value that matches
-	 * $key. The value will be added to dir.root to form a complete
-	 * directory path. If the value begins with a slash it will be
-	 * treated as an absolute path and returned explicitly. A
-	 * ConfigKeyException will be thrown if the path can't be resolved.
-	 *
-	 * @param string $key The key in the config file
-	 */
-	public function getPath($key) {
-		$path = $this->getRequired($key);
-		if(substr($path, 0, 1) === '/') {
-			return $path;
-		}
+    /**
+     * Get a configuration value that matches $key in the same way as
+     * get(), but a ConfigKeyException will be thrown if
+     * the key is not found.
+     */
+    public function getRequired($key)
+    {
+        $value = $this->get($key);
+        if (null !== $value) {
+            return $value;
+        }
+        throw new ConfigKeyException("Required value not found in Config instance '$this->name': $key");
+    }
+
+    /**
+     * Get a directory path from the configuration value that matches
+     * $key. The value will be added to dir.root to form a complete
+     * directory path. If the value begins with a slash it will be
+     * treated as an absolute path and returned explicitly. A
+     * ConfigKeyException will be thrown if the path can't be resolved.
+     *
+     * @param string $key The key in the config file
+     */
+    public function getPath($key)
+    {
+        $path = $this->getRequired($key);
+        if (substr($path, 0, 1) === '/') {
+            return $path;
+        }
         if (!$this->root_dir) {
             throw new \Exception('Root directory has not been set');
         }
 
-		return $this->root_dir . $path;
-	}
+        return $this->root_dir . $path;
+    }
 
-	/**
-	 * Get a directory path from the configuration value that matches
-	 * $key. The value will be added to the directory of this module
-	 * to form a complete directory path. If the value begins
-	 * with a slash it will be treated as an absolute path and
-	 * returned explicitly. If this config instance is
-	 * 'neptune', the result will be the same as getPath(). A
-	 * ConfigKeyException will be thrown if the path can't be
-	 * resolved.
-	 *
-	 * @param string $key The key in the config file
-	 */
-	public function getRelativePath($key) {
+    /**
+     * Get a directory path from the configuration value that matches
+     * $key. The value will be added to the directory of this module
+     * to form a complete directory path. If the value begins
+     * with a slash it will be treated as an absolute path and
+     * returned explicitly. If this config instance is
+     * 'neptune', the result will be the same as getPath(). A
+     * ConfigKeyException will be thrown if the path can't be
+     * resolved.
+     *
+     * @param string $key The key in the config file
+     */
+    public function getRelativePath($key)
+    {
         $path = $this->getRequired($key);
-        if(substr($path, 0, 1) === '/') {
+        if (substr($path, 0, 1) === '/') {
             return $path;
         }
+
         return dirname($this->filename) . '/' . $path;
-	}
+    }
 
-	/**
-	 * Get the first value from an array of configuration values that
-	 * matches $key in the same way as getFirst(), but a
-	 * ConfigKeyException will be thrown if the key is not found.
-	 */
-	public function getFirstRequired($key) {
-		$value = $this->dot_array->getFirst($key);
-		if ($value) {
-			return $value;
-		}
-		throw new ConfigKeyException("Required first value not found in Config instance '$this->name': $key");
-	}
+    /**
+     * Get the first value from an array of configuration values that
+     * matches $key in the same way as getFirst(), but a
+     * ConfigKeyException will be thrown if the key is not found.
+     */
+    public function getFirstRequired($key)
+    {
+        $value = $this->dot_array->getFirst($key);
+        if ($value) {
+            return $value;
+        }
+        throw new ConfigKeyException("Required first value not found in Config instance '$this->name': $key");
+    }
 
-	/**
-	 * Set a configuration value with $key.
-	 * $key uses the dot array syntax: parent.child.child.
-	 * If $value is an array this will also be accessible using the
-	 * dot array syntax.
-	 */
-	public function set($key, $value) {
-		$this->dot_array->set($key, $value);
-		$this->original->set($key, $value);
-		$this->modified = true;
+    /**
+     * Set a configuration value with $key.
+     * $key uses the dot array syntax: parent.child.child.
+     * If $value is an array this will also be accessible using the
+     * dot array syntax.
+     */
+    public function set($key, $value)
+    {
+        $this->dot_array->set($key, $value);
+        $this->original->set($key, $value);
+        $this->modified = true;
+
         return $this;
-	}
+    }
 
-	/**
-	 * Override values in this Config instance with values from
-	 * $array. They will not be included in toString() or save().
-	 */
-	public function override(array $array) {
-		$this->dot_array->merge($array);
-	}
+    /**
+     * Override values in this Config instance with values from
+     * $array. They will not be included in toString() or save().
+     */
+    public function override(array $array)
+    {
+        $this->dot_array->merge($array);
+    }
 
     /**
      * Get the current configuration as a string, as it would appear
@@ -162,61 +174,66 @@ class Config {
         return '<?php return ' . var_export($this->original->get(), true) . '?>';
     }
 
-	/**
-	 * Save the current configuration instance.
-	 * A ConfigFileException will be thrown if filename is not set or
-	 * if php can't write to the file.
-	 */
-	public function save($filename = null) {
-		if (false === $this->modified) {
-			return true;
-		}
+    /**
+     * Save the current configuration instance.
+     * A ConfigFileException will be thrown if filename is not set or
+     * if php can't write to the file.
+     */
+    public function save($filename = null)
+    {
+        if (false === $this->modified) {
+            return true;
+        }
         if (null === $filename) {
             $filename = $this->filename;
         }
 
-		if(!$filename) {
-			throw new ConfigFileException(
-				"Unable to save Config instance '$this->name', no filename supplied"
-			);
-		}
-		if(!file_exists($filename) && !@touch($filename)){
-			throw new ConfigFileException(
-				"Unable to create configuration file
-						$filename. Check file paths and permissions
-						are correct."
-			);
-		};
-		if(!is_writable($filename)) {
-			throw new ConfigFileException(
-				"Unable to write to configuration file
-						$filename. Check file paths and permissions
-						are correct."
-			);
-		}
-		file_put_contents($filename, $this->toString());
-		return true;
-	}
+        if (!$filename) {
+            throw new ConfigFileException(
+                "Unable to save Config instance '$this->name', no filename supplied"
+            );
+        }
+        if (!file_exists($filename) && !@touch($filename)) {
+            throw new ConfigFileException(
+                "Unable to create configuration file
+                        $filename. Check file paths and permissions
+                        are correct."
+            );
+        };
+        if (!is_writable($filename)) {
+            throw new ConfigFileException(
+                "Unable to write to configuration file
+                        $filename. Check file paths and permissions
+                        are correct."
+            );
+        }
+        file_put_contents($filename, $this->toString());
 
-	/**
-	 * Set the filename for the current configuration instance.
-	 *
-	 * @param string $filename The name of the file
-	 */
-	public function setFileName($filename) {
-		$this->filename = $filename;
-	}
+        return true;
+    }
 
-	/**
-	 * Get the filename of the current configuration instance.
-	 */
-	public function getFileName() {
-		return $this->filename;
-	}
+    /**
+     * Set the filename for the current configuration instance.
+     *
+     * @param string $filename The name of the file
+     */
+    public function setFileName($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * Get the filename of the current configuration instance.
+     */
+    public function getFileName()
+    {
+        return $this->filename;
+    }
 
     public function setRootDirectory($directory)
     {
         $this->root_dir = $directory;
+
         return $this;
     }
 
--- tests/Neptune/Tests/Config/ConfigTest.php
+++ tests/Neptune/Tests/Config/ConfigTest.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Neptune\Tests\Core;
+namespace Neptune\Tests\Config;
 
-use Neptune\Core\Config;
+use Neptune\Config\Config;
 
 use Temping\Temping;
 
@@ -12,36 +12,40 @@ require_once __DIR__ . '/../../../bootstrap.php';
  * ConfigTest
  * @author Glynn Forrest <me@glynnforrest.com>
  */
-class ConfigTest extends \PHPUnit_Framework_TestCase {
-
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
     protected $temp;
     protected $config;
 
-	public function setUp() {
-		$this->temp = new Temping();
+    public function setUp()
+    {
+        $this->temp = new Temping();
         $this->temp->init();
 
         $this->config = new Config('testing');
         $this->config->set('one', 'one');
         $this->config->set('two', array(
-			'one' => 'two-one',
-			'two' => 'two-two'
-		));
-	}
+            'one' => 'two-one',
+            'two' => 'two-two'
+        ));
+    }
 
-	public function tearDown() {
-		$this->temp->reset();
-	}
+    public function tearDown()
+    {
+        $this->temp->reset();
+    }
 
-	public function testSetAndGet() {
-		$c = new Config('testing');
+    public function testSetAndGet()
+    {
+        $c = new Config('testing');
         $this->assertSame($c, $c->set('foo', 'bar'));
-		$this->assertSame('bar', $c->get('foo'));
-	}
+        $this->assertSame('bar', $c->get('foo'));
+    }
 
-	public function testGetDefault() {
-		$this->assertSame('default', $this->config->get('fake-key', 'default'));
-	}
+    public function testGetDefault()
+    {
+        $this->assertSame('default', $this->config->get('fake-key', 'default'));
+    }
 
     public function testGetNoKey()
     {
@@ -55,24 +59,28 @@ class ConfigTest extends \PHPUnit_Framework_TestCase {
         $this->assertSame($values, $this->config->get());
     }
 
-	public function testGetFirst() {
+    public function testGetFirst()
+    {
         $this->assertSame('two-one', $this->config->getFirst('two'));
         $this->assertSame('one', $this->config->getFirst());
-	}
+    }
 
-	public function testGetFirstDefault() {
-		$this->assertSame('default', $this->config->getFirst('fake-key', 'default'));
-	}
+    public function testGetFirstDefault()
+    {
+        $this->assertSame('default', $this->config->getFirst('fake-key', 'default'));
+    }
 
-	public function testGetRequired() {
-		$this->assertSame('two-one', $this->config->getRequired('two.one'));
-	}
+    public function testGetRequired()
+    {
+        $this->assertSame('two-one', $this->config->getRequired('two.one'));
+    }
 
-	public function testGetRequiredThrowsException() {
-		$msg = "Required value not found in Config instance 'testing': fake";
-		$this->setExpectedException('Neptune\\Exceptions\\ConfigKeyException', $msg);
-		$this->config->getRequired('fake');
-	}
+    public function testGetRequiredThrowsException()
+    {
+        $msg = "Required value not found in Config instance 'testing': fake";
+        $this->setExpectedException('Neptune\\Exceptions\\ConfigKeyException', $msg);
+        $this->config->getRequired('fake');
+    }
 
     public function testGetRequiredEmptyString()
     {
@@ -80,75 +88,84 @@ class ConfigTest extends \PHPUnit_Framework_TestCase {
         $this->assertSame('', $this->config->getRequired('string'));
     }
 
-	public function testGetFirstRequired() {
-		$this->assertSame('two-one', $this->config->getFirstRequired('two'));
-	}
+    public function testGetFirstRequired()
+    {
+        $this->assertSame('two-one', $this->config->getFirstRequired('two'));
+    }
 
-	public function testGetFirstRequiredThrowsException() {
-		$msg = "Required first value not found in Config instance 'testing': fake";
-		$this->setExpectedException('Neptune\\Exceptions\\ConfigKeyException', $msg);
-		$this->config->getFirstRequired('fake');
-	}
+    public function testGetFirstRequiredThrowsException()
+    {
+        $msg = "Required first value not found in Config instance 'testing': fake";
+        $this->setExpectedException('Neptune\\Exceptions\\ConfigKeyException', $msg);
+        $this->config->getFirstRequired('fake');
+    }
 
-	/**
-	 * Throw an exception if there is no array to get first value from.
-	 */
-	public function testGetFirstRequiredThrowsExceptionNoArray() {
-		$this->config->set('3.1', 'not-an-array');
-		$msg = "Required first value not found in Config instance 'testing': 3.1";
-		$this->setExpectedException('Neptune\\Exceptions\\ConfigKeyException', $msg);
-		$this->config->getFirstRequired('3.1');
-	}
+    /**
+     * Throw an exception if there is no array to get first value from.
+     */
+    public function testGetFirstRequiredThrowsExceptionNoArray()
+    {
+        $this->config->set('3.1', 'not-an-array');
+        $msg = "Required first value not found in Config instance 'testing': 3.1";
+        $this->setExpectedException('Neptune\\Exceptions\\ConfigKeyException', $msg);
+        $this->config->getFirstRequired('3.1');
+    }
 
-	public function testSetNested() {
-		$c = new Config('fake');
-		$c->set('parent.child', 'value');
-		$this->assertSame(array('parent' => array('child' => 'value')), $c->get());
-	}
+    public function testSetNested()
+    {
+        $c = new Config('fake');
+        $c->set('parent.child', 'value');
+        $this->assertSame(array('parent' => array('child' => 'value')), $c->get());
+    }
 
-	public function testGetNested() {
-		$c = new Config('fake');
-		$c->set('parent', array('child' => 'value'));
-		$this->assertSame('value', $c->get('parent.child'));
-	}
+    public function testGetNested()
+    {
+        $c = new Config('fake');
+        $c->set('parent', array('child' => 'value'));
+        $this->assertSame('value', $c->get('parent.child'));
+    }
 
-	public function testSetDeepNested() {
-		$c = new Config('fake');
-		$c->set('parent.child.0.1.2.3.4', 'value');
-		$this->assertSame(array('parent' => array('child' => array(
-			0 => array(1 => array(2 => array(3 => array(4 =>'value'))))))), $c->get());
-	}
+    public function testSetDeepNested()
+    {
+        $c = new Config('fake');
+        $c->set('parent.child.0.1.2.3.4', 'value');
+        $this->assertSame(array('parent' => array('child' => array(
+            0 => array(1 => array(2 => array(3 => array(4 =>'value'))))))), $c->get());
+    }
 
-	public function testGetDeepNested() {
-		$c = new Config('fake');
-		$c->set('parent', array('child' => array(
-			0 => array(1 => array(2 => array(3 => array(4 =>'value')))))));
-		$this->assertSame('value', $c->get('parent.child.0.1.2.3.4'));
-	}
+    public function testGetDeepNested()
+    {
+        $c = new Config('fake');
+        $c->set('parent', array('child' => array(
+            0 => array(1 => array(2 => array(3 => array(4 =>'value')))))));
+        $this->assertSame('value', $c->get('parent.child.0.1.2.3.4'));
+    }
 
-	/**
-	 * Strip out whitespace and new lines from config settings to make
-	 * them easier to compare against.
-	 */
-	protected function removeWhitespace($content) {
-		return preg_replace('`\s+`', '', $content);
-	}
+    /**
+     * Strip out whitespace and new lines from config settings to make
+     * them easier to compare against.
+     */
+    protected function removeWhitespace($content)
+    {
+        return preg_replace('`\s+`', '', $content);
+    }
 
-	public function testSave() {
-		$this->config->set('foo', 'bar');
+    public function testSave()
+    {
+        $this->config->set('foo', 'bar');
         $filename = $this->temp->getPathname('foo.php');
-		$this->config->save($filename);
-		$this->assertSame($this->removeWhitespace($this->config->toString()),
-							$this->removeWhitespace($this->temp->getContents('foo.php')));
-	}
+        $this->config->save($filename);
+        $this->assertSame($this->removeWhitespace($this->config->toString()),
+                            $this->removeWhitespace($this->temp->getContents('foo.php')));
+    }
 
     public function testSaveNoFilename()
     {
-		$this->config->set('foo', 'bar');
+        $this->config->set('foo', 'bar');
         $this->config->setFilename($this->temp->getPathname('bar.php'));
-		$this->config->save();
-		$this->assertSame($this->removeWhitespace($this->config->toString()),
-							$this->removeWhitespace($this->temp->getContents('bar.php')));
+        $this->config->save();
+        $this->assertSame($this->removeWhitespace($this->config->toString()),
+                            $this->removeWhitespace($this->temp->getContents('bar.php')));
     }
 
     public function testSaveUsesFilename()
@@ -165,13 +182,14 @@ class ConfigTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue($this->temp->exists('actually-here.php'));
     }
 
-	public function testSaveThrowsExceptionWithNoFile() {
-		$c = new Config('ad-hoc');
-		$c->set('key', 'value');
-		$msg = "Unable to save Config instance 'ad-hoc', no filename supplied";
-		$this->setExpectedException('\\Neptune\\Exceptions\\ConfigFileException', $msg);
-		$c->save();
-	}
+    public function testSaveThrowsExceptionWithNoFile()
+    {
+        $c = new Config('ad-hoc');
+        $c->set('key', 'value');
+        $msg = "Unable to save Config instance 'ad-hoc', no filename supplied";
+        $this->setExpectedException('\\Neptune\\Exceptions\\ConfigFileException', $msg);
+        $c->save();
+    }
 
     public function testUnreadableConfig()
     {
@@ -193,59 +211,66 @@ class ConfigTest extends \PHPUnit_Framework_TestCase {
         $c->save($file);
     }
 
-	public function testSaveDoesNotWriteIfNotModified() {
-		$file = $this->temp->getPathname('do-not-write.php');
-		$c = new Config('new');
+    public function testSaveDoesNotWriteIfNotModified()
+    {
+        $file = $this->temp->getPathname('do-not-write.php');
+        $c = new Config('new');
         $c->setFilename($file);
-		$c->save();
-		$this->assertFalse(file_exists($file));
-	}
+        $c->save();
+        $this->assertFalse(file_exists($file));
+    }
 
-	public function testSetAndGetFilename() {
-		$c = new Config('testing');
+    public function testSetAndGetFilename()
+    {
+        $c = new Config('testing');
         $c->setFilename('test');
-		$this->assertSame('test', $c->getFileName());
-	}
+        $this->assertSame('test', $c->getFileName());
+    }
 
-	public function testOverride() {
+    public function testOverride()
+    {
         $this->assertSame('one', $this->config->get('one'));
         $this->config->override(array(
-			'one' => 'override',
-			'two' => array(
-				'three' => 'two-three'
-			)
-		));
+            'one' => 'override',
+            'two' => array(
+                'three' => 'two-three'
+            )
+        ));
         $this->assertSame('override', $this->config->get('one'));
         $this->assertSame('two-one', $this->config->get('two.one'));
         $this->assertSame('two-three', $this->config->get('two.three'));
     }
 
-	public function testGetPath() {
-		$neptune = new Config('neptune');
-		$neptune->setRootDirectory('/path/to/root/');
-		$neptune->set('some.dir', 'my-dir');
-		$this->assertSame('/path/to/root/my-dir', $neptune->getPath('some.dir'));
-	}
+    public function testGetPath()
+    {
+        $neptune = new Config('neptune');
+        $neptune->setRootDirectory('/path/to/root/');
+        $neptune->set('some.dir', 'my-dir');
+        $this->assertSame('/path/to/root/my-dir', $neptune->getPath('some.dir'));
+    }
 
-	public function testGetPathAbsolute() {
-		$neptune = new Config('neptune');
-		$neptune->set('some.absolute.dir', '/my-dir');
-		$this->assertSame('/my-dir', $neptune->getPath('some.absolute.dir'));
-	}
+    public function testGetPathAbsolute()
+    {
+        $neptune = new Config('neptune');
+        $neptune->set('some.absolute.dir', '/my-dir');
+        $this->assertSame('/my-dir', $neptune->getPath('some.absolute.dir'));
+    }
 
-	public function testGetRelativePath() {
-		$module = new Config('module');
+    public function testGetRelativePath()
+    {
+        $module = new Config('module');
         $module->setFilename('/some/path/to/module/config.php');
-		$module->set('assets.dir', 'assets/');
-		$this->assertSame('/some/path/to/module/assets/', $module->getRelativePath('assets.dir'));
-	}
+        $module->set('assets.dir', 'assets/');
+        $this->assertSame('/some/path/to/module/assets/', $module->getRelativePath('assets.dir'));
+    }
 
-	public function testGetRelativePathAbsolute() {
-		$module = new Config('module');
+    public function testGetRelativePathAbsolute()
+    {
+        $module = new Config('module');
         $module->setFilename('/some/path/to/module/config.php');
-		$module->set('some.absolute.dir', '/my-dir');
-		$this->assertSame('/my-dir', $module->getRelativePath('some.absolute.dir'));
-	}
+        $module->set('some.absolute.dir', '/my-dir');
+        $this->assertSame('/my-dir', $module->getRelativePath('some.absolute.dir'));
+    }
 
     public function testToString()
     {
