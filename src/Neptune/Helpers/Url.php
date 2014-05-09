3510e3a185d828bcb124e6b2cbb3221740ca7c26
Glynn Forrest <me@glynnforrest.com>
2014-05-09 01:15:04 +0100

    Rewriting Url to not use Config.

--- src/Neptune/Helpers/Url.php
+++ src/Neptune/Helpers/Url.php
@@ -2,22 +2,33 @@
 
 namespace Neptune\Helpers;
 
-use Neptune\Core\Config;
-
 /**
  * Url
  * @author Glynn Forrest me@glynnforrest.com
  **/
-class Url {
+class Url
+{
+    protected $root_url;
 
-	public static function to($url, $protocol = 'http') {
-		if(strpos($url, '://')) {
-			return $url;
-		}
-		if(substr($url, 0, 1) == '/') {
-			$url = substr($url, 1);
-		}
-		return $protocol . '://' . Config::load()->getRequired('root_url') . $url;
-	}
+    public function __construct($root_url)
+    {
+        //make sure url has a trailing slash
+        if (substr($root_url, -1) !== '/') {
+            $root_url .= '/';
+        }
+        $this->root_url = $root_url;
+    }
+
+    public function to($url, $protocol = 'http')
+    {
+        if (strpos($url, '://')) {
+            return $url;
+        }
+        if (substr($url, 0, 1) == '/') {
+            $url = substr($url, 1);
+        }
+
+        return $protocol . '://' . $this->root_url . $url;
+    }
 
 }
--- tests/Neptune/Tests/Helpers/UrlTest.php
+++ tests/Neptune/Tests/Helpers/UrlTest.php
@@ -3,7 +3,6 @@
 namespace Neptune\Tests\Helpers;
 
 use Neptune\Helpers\Url;
-use Neptune\Core\Config;
 
 require_once __DIR__ . '/../../../bootstrap.php';
 
@@ -11,29 +10,28 @@ require_once __DIR__ . '/../../../bootstrap.php';
  * UrlTest
  * @author Glynn Forrest me@glynnforrest.com
  **/
-class UrlTest extends \PHPUnit_Framework_TestCase {
+class UrlTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->url = new Url('myapp.local');
+    }
 
-	public function setUp() {
-		$c = Config::create('testing');
-		$c->set('root_url', 'myapp.local/');
-	}
+    public function testTo()
+    {
+        $this->assertEquals('http://myapp.local/404', $this->url->to('404'));
+    }
 
-	public function tearDown() {
-		Config::unload();
-	}
+    public function testToFtp()
+    {
+        $this->assertEquals('ftp://myapp.local/file', $this->url->to('file', 'ftp'));
+    }
 
-	public function testTo() {
-		$this->assertEquals('http://myapp.local/404', Url::to('404'));
-	}
-
-	public function testToFtp() {
-		$this->assertEquals('ftp://myapp.local/file', Url::to('file', 'ftp'));
-	}
-
-	public function testToAbsolute() {
-		$this->assertEquals('http://google.com', Url::to('http://google.com'));
-		$this->assertEquals('https://google.com', Url::to('https://google.com'));
-		$this->assertEquals('ftp://google.com', Url::to('ftp://google.com'));
-	}
+    public function testToAbsolute()
+    {
+        $this->assertEquals('http://google.com', $this->url->to('http://google.com'));
+        $this->assertEquals('https://google.com', $this->url->to('https://google.com'));
+        $this->assertEquals('ftp://google.com', $this->url->to('ftp://google.com'));
+    }
 
 }
