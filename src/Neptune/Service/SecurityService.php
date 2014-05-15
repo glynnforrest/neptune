d72510d57ffc473acc5df30f96a51cd05efdf33b
Glynn Forrest <me@glynnforrest.com>
2014-05-15 09:34:12 +0100

    Update Config usage in services.

--- src/Neptune/Service/SecurityService.php
+++ src/Neptune/Service/SecurityService.php
@@ -3,7 +3,7 @@
 namespace Neptune\Service;
 
 use Neptune\Core\Neptune;
-use Neptune\Core\Config;
+use Neptune\Config\Config;
 use Neptune\Security\Firewall;
 use Neptune\Security\SecurityFactory;
 use Neptune\Security\Csrf\CsrfManager;
