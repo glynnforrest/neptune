1ce2ab3c2efffd6130df9f0aa2d37a69f18f0471
Glynn Forrest <me@glynnforrest.com>
2014-05-28 13:28:13 -0500

    Adding assets and security extension to view service.

--- src/Neptune/Service/ViewService.php
+++ src/Neptune/Service/ViewService.php
@@ -5,6 +5,9 @@ namespace Neptune\Service;
 use Neptune\Core\Neptune;
 use Neptune\View\ViewCreator;
 
+use Neptune\View\Extension\AssetsExtension;
+use Neptune\View\Extension\SecurityExtension;
+
 /**
  * ViewService
  *
@@ -16,7 +19,17 @@ class ViewService implements ServiceInterface
     public function register(Neptune $neptune)
     {
         $neptune['view'] = function($neptune) {
-            return new ViewCreator($neptune);
+            $creator = new ViewCreator($neptune);
+
+            if ($neptune->offsetExists('assets')) {
+                $creator->addExtension(new AssetsExtension($neptune['assets']));
+            }
+
+            if ($neptune->offsetExists('security')) {
+                $creator->addExtension(new SecurityExtension($neptune['security']));
+            }
+
+            return $creator;
         };
     }
 
