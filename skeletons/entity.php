6bfaebabe12c0f8235a1bca9142b3d1404f88e4e
Glynn Forrest <me@glynnforrest.com>
2014-06-02 15:18:15 -0500

    Updating entity skeleton.

--- skeletons/entity.php
+++ skeletons/entity.php
@@ -2,20 +2,15 @@
 namespace <?=$this->namespace;?>\Entity;
 
 use Neptune\Database\Entity\Entity;
-use Neptune\Database\SQLQuery;
 
 <?=$this->class_info;?>
-class <?=$this->entity_name;?> extends Entity {
+class <?=$this->entity_name;?> extends Entity
+{
 
-	protected static $table = '<?=$this->table;?>';
-	protected static $fields = array();
-	protected static $rules = array();
-	protected static $relations = array();
-
-	public static function buildForm($action = null, $values = array(), $errors = array(), $method = 'POST') {
-		$f = parent::buildForm($action, $values, $errors, $method);
-        //customise the form here
-		return $f;
-	}
+    protected static $table = '<?=$this->table;?>';
+    protected static $fields = array(
+        'id',
+    );
+    protected static $relations = array();
 
 }
