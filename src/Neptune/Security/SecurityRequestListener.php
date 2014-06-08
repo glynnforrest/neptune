059abda60c944f5aba263d7f8d8731f8582116ea
Glynn Forrest <me@glynnforrest.com>
2014-06-08 09:56:46 -0500

    Adding SecurityRequestListener.

---
+++ src/Neptune/Security/SecurityRequestListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Neptune\Security;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * SecurityRequestListener assigns the current Request to the
+ * SecurityFactory automatically. This enables security drivers to
+ * have access to the request without assigning it manually.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SecurityRequestListener implements EventSubscriberInterface
+{
+
+    protected $factory;
+
+    public function __construct(SecurityFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $this->factory->setRequest($event->getRequest());
+
+        return true;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST => array('onKernelRequest')
+        );
+    }
+
+}
---
+++ tests/Neptune/Tests/EventListener/SecurityRequestListenerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Neptune\Tests\EventListener;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+use Neptune\Security\SecurityRequestListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * SecurityRequestListenerTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SecurityRequestListenerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        $this->factory = $this->getMockBuilder('Neptune\Security\SecurityFactory')
+                              ->disableOriginalConstructor()
+                              ->getMock();
+        $this->listener = new SecurityRequestListener($this->factory);
+    }
+
+    public function testOnKernelRequest()
+    {
+        $request = new Request();
+        $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseEvent')
+                      ->disableOriginalConstructor()
+                      ->getMock();
+
+        $event->expects($this->once())
+              ->method('getRequest')
+              ->will($this->returnValue($request));
+
+        $this->factory->expects($this->once())
+                      ->method('setRequest')
+                      ->with($request);
+
+        $this->listener->onKernelRequest($event);
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        $expected = array(KernelEvents::REQUEST => array('onKernelRequest'));
+        $this->assertSame($expected, SecurityRequestListener::getSubscribedEvents());
+    }
+
+}
