c531b63c6556216928ef91ed6979ac4b6adfa030
Glynn Forrest <me@glynnforrest.com>
2014-06-11 16:01:00 -0500

    Using appropriate variable name in EntityBuilder.

--- src/Neptune/Database/Entity/EntityBuilder.php
+++ src/Neptune/Database/Entity/EntityBuilder.php
@@ -13,27 +13,29 @@ class EntityBuilder
 {
 
     protected $query;
-    protected $thing_class;
+    protected $entity_class;
     protected $eager = array();
 
-    public function __construct(AbstractQuery $query, $thing_class)
+    public function __construct(AbstractQuery $query, $entity_class)
     {
         if ($query->getType()  !== 'SELECT') {
             throw new \InvalidArgumentException("EntityBuilder only supports SELECT queries.");
         }
         $this->query = $query;
-        $this->thing_class = $thing_class;
+        $this->entity_class = $entity_class;
     }
 
     public function __call($method, array $args)
     {
         call_user_func_array(array($this->query, $method), $args);
+
         return $this;
     }
 
     public function execute()
     {
-        $class = $this->thing_class;
+        $class = $this->entity_class;
+
         return $class::selectFromQuery($this->query, $this->eager);
     }
 
