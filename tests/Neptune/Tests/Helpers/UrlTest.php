b933f45dedd59c29084862c5c0a8c14326d55f8e
Glynn Forrest <me@glynnforrest.com>
2014-07-16 23:46:53 -0500

    Enforcing stricter UrlTest assertions.

--- tests/Neptune/Tests/Helpers/UrlTest.php
+++ tests/Neptune/Tests/Helpers/UrlTest.php
@@ -19,19 +19,19 @@ class UrlTest extends \PHPUnit_Framework_TestCase
 
     public function testTo()
     {
-        $this->assertEquals('http://myapp.local/404', $this->url->to('404'));
+        $this->assertSame('http://myapp.local/404', $this->url->to('404'));
     }
 
     public function testToFtp()
     {
-        $this->assertEquals('ftp://myapp.local/file', $this->url->to('file', 'ftp'));
+        $this->assertSame('ftp://myapp.local/file', $this->url->to('file', 'ftp'));
     }
 
     public function testToAbsolute()
     {
-        $this->assertEquals('http://google.com', $this->url->to('http://google.com'));
-        $this->assertEquals('https://google.com', $this->url->to('https://google.com'));
-        $this->assertEquals('ftp://google.com', $this->url->to('ftp://google.com'));
+        $this->assertSame('http://google.com', $this->url->to('http://google.com'));
+        $this->assertSame('https://google.com', $this->url->to('https://google.com'));
+        $this->assertSame('ftp://google.com', $this->url->to('ftp://google.com'));
     }
 
     public function testToEmptyString()
