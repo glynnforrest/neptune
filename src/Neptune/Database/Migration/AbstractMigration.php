1e16f9ad905d13b5c38e3f3a55c06c21e3b3ffb7
Glynn Forrest <me@glynnforrest.com>
2014-08-05 11:52:05 +0100

    Greatly simplifying AbstractMigration.

--- src/Neptune/Database/Migration/AbstractMigration.php
+++ src/Neptune/Database/Migration/AbstractMigration.php
@@ -4,6 +4,7 @@ namespace Neptune\Database\Migration;
 
 use Neptune\Core\Neptune;
 use Neptune\Core\NeptuneAwareInterface;
+use Doctrine\DBAL\Schema\Schema;
 
 /**
  * AbstractMigration
@@ -14,7 +15,6 @@ abstract class AbstractMigration implements NeptuneAwareInterface
 {
 
     protected $description;
-    protected $sql = array();
     protected $neptune;
 
     public function setNeptune(Neptune $neptune)
@@ -27,52 +27,13 @@ abstract class AbstractMigration implements NeptuneAwareInterface
         return $this->neptune;
     }
 
-    abstract public function up();
+    abstract public function up(Schema $schema);
 
-    abstract public function down();
-
-    protected function addSql($sql)
-    {
-        $this->sql[] = $sql;
-    }
-
-    public function getSql()
-    {
-        return $this->sql;
-    }
+    abstract public function down(Schema $schema);
 
     public function getVersion()
     {
         return substr(get_class($this), -14);
-    }
-
-    public function isRequiredUp($current, $version)
-    {
-        $migration_version = $this->getVersion();
-        if ($migration_version < $current) {
-            return false;
-        }
-        if ($migration_version > $version) {
-            return false;
-        }
-        return true;
-    }
-
-    public function isRequiredDown($current, $version)
-    {
-        $migration_version = $this->getVersion();
-        if ($migration_version > $current) {
-            return false;
-        }
-        if ($migration_version < $version) {
-            return false;
-        }
-        return true;
-    }
-
-    public function getDescription()
-    {
-        return $this->description;
     }
 
 }
