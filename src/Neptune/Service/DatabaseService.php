029d908a8589f714fed21eafad2b8c37c024a75a
Glynn Forrest <me@glynnforrest.com>
2014-08-26 21:18:11 +0100

    DatabaseService now uses Doctrine's DriverManager.

--- src/Neptune/Service/DatabaseService.php
+++ src/Neptune/Service/DatabaseService.php
@@ -3,7 +3,10 @@
 namespace Neptune\Service;
 
 use Neptune\Core\Neptune;
-use Neptune\Database\DatabaseFactory;
+use Neptune\Exceptions\ConfigKeyException;
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Configuration;
 
 /**
  * DatabaseService
@@ -15,13 +18,31 @@ class DatabaseService implements ServiceInterface
 
     public function register(Neptune $neptune)
     {
+        $config = $neptune['config'];
 
-        $neptune['database.factory'] = function($neptune) {
-            return new DatabaseFactory($neptune['config'], $neptune);
-        };
+        $names = array_keys($config->get('database', []));
+        if (empty($names)) {
+            throw new ConfigKeyException('Database configuration is empty');
+        }
 
-        $neptune['db'] = function($neptune) {
-            return $neptune['database.factory']->get();
+        foreach ($names as $name) {
+            $neptune["db.$name"] = function ($neptune) use ($name, $config) {
+                $configuration = new Configuration();
+
+                $logger = $config->get("database.$name.logger", false);
+                if ($logger) {
+                    $configuration->setSQLLogger(new \Neptune\Database\PsrSqlLogger($neptune[$logger]));
+                }
+
+                return DriverManager::getConnection($config->getRequired("database.$name"), $configuration);
+            };
+        }
+
+        //shortcut for the first database
+        $neptune['db'] = function ($neptune) use ($names) {
+            $name = $names[0];
+
+            return $neptune["db.$name"];
         };
     }
 
