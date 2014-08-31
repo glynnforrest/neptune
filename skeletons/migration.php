e0b0b79ee174cfb3cfbe17965566782f4e2fb290
Glynn Forrest <me@glynnforrest.com>
2014-08-31 18:47:49 +0100

    create:migration now prompts for a description.

--- skeletons/migration.php
+++ skeletons/migration.php
@@ -2,18 +2,19 @@
 namespace <?=$this->namespace;?>\Migrations;
 
 use Neptune\Database\Migration\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
 
 <?=$this->class_info;?>
 class <?=$this->class_name;?> extends AbstractMigration {
 
-    protected $description = '<?=$this->class_name;?>';
+    protected $description = '<?=$this->description;?>';
 
-    public function up()
+    public function up(Schema $schema)
     {
 
     }
 
-    public function down()
+    public function down(Schema $schema)
     {
 
     }
--- src/Neptune/Command/CreateMigrationCommand.php
+++ src/Neptune/Command/CreateMigrationCommand.php
@@ -4,6 +4,11 @@ namespace Neptune\Command;
 
 use Neptune\View\Skeleton;
 
+use Stringy\Stringy;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
 /**
  * CreateMigrationCommand
  * @author Glynn Forrest <me@glynnforrest.com>
@@ -13,6 +18,25 @@ class CreateMigrationCommand extends CreateCommand
 
     protected $name = 'create:migration';
     protected $description = 'Create a new migration';
+    protected $migration_description;
+
+    protected function configure()
+    {
+        $this->setName($this->name)
+             ->setDescription($this->description)
+             ->addArgument(
+                 'description',
+                 InputArgument::OPTIONAL,
+                 'The description of the migration.'
+             )
+             ->addOption(
+                 'module',
+                 'm',
+                 InputOption::VALUE_REQUIRED,
+                 'The module to place the file.',
+                 $this->getDefaultModule()
+             );
+    }
 
     protected function getTargetPath($name)
     {
@@ -21,13 +45,22 @@ class CreateMigrationCommand extends CreateCommand
 
     protected function getResourceName()
     {
-        return 'Migration' . date('YmdHis') ;
+        $description = $this->input->getArgument('description');
+        if (!$description) {
+            $dialog = $this->getHelper('dialog');
+            $description = $dialog->ask($this->output, 'Description for this migration: ');
+        }
+
+        $this->migration_description = str_replace("'", "\'", $description);
+
+        return 'Migration' . date('YmdHis') . Stringy::create($description)->slugify()->upperCamelize();
     }
 
     protected function getSkeleton($name)
     {
         $skeleton = new Skeleton($this->getSkeletonPath('migration'));
         $skeleton->class_name = $name;
+        $skeleton->description = $this->migration_description;
 
         return $skeleton;
     }
