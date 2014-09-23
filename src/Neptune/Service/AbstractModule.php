cbf5e16d8a5dd1de259acbaac2f7253006357c25
Glynn Forrest <me@glynnforrest.com>
2014-09-23 09:43:38 +0100

    Changing AbstractModule to be aware of route prefix.

--- src/Neptune/Service/AbstractModule.php
+++ src/Neptune/Service/AbstractModule.php
@@ -14,7 +14,39 @@ use Neptune\Core\Neptune;
 abstract class AbstractModule implements ServiceInterface
 {
 
-    abstract public function routes(Router $router, $prefix, Neptune $neptune);
+    protected $route_prefix;
+
+    public function __construct($route_prefix = '')
+    {
+        $this->route_prefix = $route_prefix;
+    }
+
+    /**
+     * Set the route prefix for the module. Pass false to disable routing.
+     *
+     * @param string|bool $route_prefix The prefix
+     */
+    public function routePrefix($route_prefix)
+    {
+        $this->route_prefix = $route_prefix;
+    }
+
+    /**
+     * Load routes for this module.
+     *
+     * @param Route $router
+     * @param Neptune $neptune
+     */
+    public function loadRoutes(Router $router, Neptune $neptune)
+    {
+        if ($this->route_prefix !== false) {
+            $this->routes($router, $this->route_prefix, $neptune);
+        }
+    }
+
+    protected function routes(Router $router, $prefix, Neptune $neptune) {
+        //add routes in child classes
+    }
 
     abstract public function getName();
 
