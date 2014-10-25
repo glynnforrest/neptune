95a8e76e13a9dad78dbc18d3165415a1817c4e93
Glynn Forrest <me@glynnforrest.com>
2014-10-25 23:46:13 +0100

    Updating env:switch command.

--- src/Neptune/Command/EnvSwitchCommand.php
+++ src/Neptune/Command/EnvSwitchCommand.php
@@ -2,49 +2,51 @@
 
 namespace Neptune\Command;
 
-use Neptune\Command\EnvListCommand;
-use Neptune\Console\Console;
-
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-
-use \DirectoryIterator;
 
 /**
  * EnvSwitchCommand
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class EnvSwitchCommand extends EnvListCommand {
+class EnvSwitchCommand extends EnvListCommand
+{
+    protected $name = 'env:switch';
+    protected $description = 'Switch to a different application environment';
 
-	protected $name = 'env:switch';
-	protected $description = 'Switch to a different application environment';
+    protected function configure()
+    {
+        $this->setName($this->name)
+             ->setDescription($this->description)
+             ->addArgument(
+                 'name',
+                 InputArgument::OPTIONAL,
+                 'The name of the environment to switch to.'
+             );
+    }
 
-	protected function configure() {
-		$this->setName($this->name)
-			 ->setDescription($this->description)
-			 ->addArgument(
-				 'name',
-				 InputArgument::OPTIONAL,
-				 'The name of the environment to switch to.'
-			 );
-	}
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        $envs = $this->getEnvs();
 
-	public function go(Console $console) {
-		$name = $this->input->getArgument('name');
-		$dialog = $this->getHelper('dialog');
-		$envs = $this->getEnvs();
-		if(!$name) {
-			$index = $dialog->select($this->output, 'Select environment:', $this->getEnvsHighlightCurrent());
-			$name = $envs[$index];
-		}
-		if(!in_array($name, $envs)) {
-			$this->output->writeln("<error>Environment not found: $name</error>");
-			return false;
-		}
-		$this->config->set('env', $name);
-		$this->config->save();
-		$this->output->writeln("Switched to <info>$name</info> environment.");
-		return true;
-	}
+        if (!$name) {
+            $dialog = $this->getHelper('dialog');
+            $index = $dialog->select($output, 'Select environment:', $this->getEnvsHighlightCurrent());
+            $name = $envs[$index];
+        }
+
+        if (!in_array($name, $envs)) {
+            $output->writeln("<error>Environment not found: $name</error>");
+
+            return false;
+        }
+
+        $this->config->set('env', $name);
+        $this->config->save();
+        $output->writeln("Switched to <info>$name</info> environment.");
+    }
 
 }
