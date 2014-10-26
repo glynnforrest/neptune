db39bc68911924d2a93e5a1b5509390f9759cdf8
Glynn Forrest <me@glynnforrest.com>
2014-10-26 17:39:18 +0000

    Updating env:remove command.

--- src/Neptune/Command/EnvRemoveCommand.php
+++ src/Neptune/Command/EnvRemoveCommand.php
@@ -2,58 +2,62 @@
 
 namespace Neptune\Command;
 
-use Neptune\Command\EnvListCommand;
-use Neptune\Console\Console;
-
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-
-use \DirectoryIterator;
 
 /**
  * EnvRemoveCommand
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class EnvRemoveCommand extends EnvListCommand {
+class EnvRemoveCommand extends EnvListCommand
+{
+    protected $name = 'env:remove';
+    protected $description = 'Remove an application environment';
 
-	protected $name = 'env:remove';
-	protected $description = 'Remove an application environment';
+    protected function configure()
+    {
+        $this->setName($this->name)
+             ->setDescription($this->description)
+             ->addArgument(
+                 'name',
+                 InputArgument::OPTIONAL,
+                 'The name of the new environment.'
+             );
+    }
 
-	protected function configure() {
-		$this->setName($this->name)
-			 ->setDescription($this->description)
-			 ->addArgument(
-				 'name',
-				 InputArgument::OPTIONAL,
-				 'The name of the new environment.'
-			 );
-	}
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        $dialog = $this->getHelper('dialog');
 
-	public function go(Console $console) {
-		$name = $this->input->getArgument('name');
-		$dialog = $this->getHelper('dialog');
-		if(!$name) {
-			$index = $dialog->select($this->output, 'Remove environment:', $this->getEnvsHighlightCurrent());
-			$name = $this->getEnvs()[$index];
-		}
-		$overwrite = $dialog->askConfirmation($this->output, "Are you sure you want to remove the environment <info>$name</info>? ", false);
-		if($overwrite) {
-			$this->removeEnv($name);
-		}
-	}
+        if (!$name) {
+            $index = $dialog->select($output, 'Remove environment:', $this->getEnvsHighlightCurrent());
+            $name = $this->getEnvs()[$index];
+        }
 
-    protected function removeEnv($name)
+        $prompt = "Are you sure you want to remove the environment <info>$name</info>? ";
+        if ($dialog->askConfirmation($output, $prompt, false)) {
+            $this->removeEnv($name, $output);
+        }
+    }
+
+    protected function removeEnv($name, OutputInterface $output)
     {
         $config = $this->getRootDirectory() . 'config/env/' . $name . '.php';
-        if(!file_exists($config)) {
-            $this->output->writeln("<error>$config not found</error>");
+        if (!file_exists($config)) {
+            $output->writeln("<error>$config not found</error>");
+
             return false;
         }
-        if(unlink($config)) {
-            $this->output->writeln("Deleted <info>$config</info>");
+        if (unlink($config)) {
+            $output->writeln("Deleted <info>$config</info>");
+
             return true;
         }
-        $this->output->writeln("<error>Unable to delete <info>$config</info>");
+        $output->writeln("<error>Unable to delete <info>$config</info>");
+
         return false;
     }
 
