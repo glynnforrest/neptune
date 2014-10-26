e9c398944d105c097d123b7a694bebf3ee03d6e0
Glynn Forrest <me@glynnforrest.com>
2014-10-26 18:03:49 +0000

    Cleanup env:list command.

--- src/Neptune/Command/EnvListCommand.php
+++ src/Neptune/Command/EnvListCommand.php
@@ -2,9 +2,6 @@
 
 namespace Neptune\Command;
 
-use Neptune\Command\Command;
-use Neptune\Console\Console;
-
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -15,39 +12,44 @@ use \DirectoryIterator;
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class EnvListCommand extends Command {
+class EnvListCommand extends Command
+{
+    protected $name = 'env:list';
+    protected $description = 'List all application environments';
 
-	protected $name = 'env:list';
-	protected $description = 'List all application environments';
-
-    public function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         foreach ($this->getEnvsHighlightCurrent() as $env) {
             $output->writeln($env);
         }
     }
 
-	protected function getEnvs() {
-		$envs = array();
-		$env_dir = $this->getRootDirectory() . 'config/env';
-		$i = new DirectoryIterator($env_dir);
-		foreach ($i as $file) {
-			if($file->isFile()) {
-				$envs[] = $file->getBasename('.php');
-			}
-		}
-		sort($envs);
-		return $envs;
-	}
+    protected function getEnvs()
+    {
+        $envs = array();
+        $env_dir = $this->getRootDirectory() . 'config/env';
+        $i = new DirectoryIterator($env_dir);
+        foreach ($i as $file) {
+            if ($file->isFile()) {
+                $envs[] = $file->getBasename('.php');
+            }
+        }
+        sort($envs);
 
-	protected function getEnvsHighlightCurrent() {
-		$current_env = $this->config->get('env');
-		return array_map(function($env) use ($current_env) {
-			if($env === $current_env) {
-				return "<info>$env</info>";
-			}
-			return $env;
-		}, $this->getEnvs());
-	}
+        return $envs;
+    }
+
+    protected function getEnvsHighlightCurrent()
+    {
+        $current_env = $this->config->get('env');
+
+        return array_map(function ($env) use ($current_env) {
+            if ($env === $current_env) {
+                return "<info>$env</info>";
+            }
+
+            return $env;
+        }, $this->getEnvs());
+    }
 
 }
