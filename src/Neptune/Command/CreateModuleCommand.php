5bfc399285de221c457177acbabc47271814349f
Glynn Forrest <me@glynnforrest.com>
2014-10-26 20:42:58 +0000

    Updating create:module command.

--- src/Neptune/Command/CreateModuleCommand.php
+++ src/Neptune/Command/CreateModuleCommand.php
@@ -2,12 +2,11 @@
 
 namespace Neptune\Command;
 
-use Neptune\Command\Command;
-use Neptune\Console\Console;
 use Neptune\Config\Config;
 
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 use Stringy\StaticStringy as S;
 
@@ -15,88 +14,80 @@ use Stringy\StaticStringy as S;
  * CreateModuleCommand
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class CreateModuleCommand extends Command {
+class CreateModuleCommand extends Command
+{
+    protected $name = 'create:module';
+    protected $description = 'Create a new module';
+    protected $module_directory;
 
-	protected $name = 'create:module';
-	protected $description = 'Create a new module';
-	protected $dirs = array(
-		'Controller',
-		'Model',
-		'Entity',
-		'Command',
-		'views',
-		'assets'
-	);
-	protected $module_directory;
+    protected function configure()
+    {
+        $this->setName($this->name)
+             ->setDescription($this->description)
+             ->addArgument(
+                 'name',
+                 InputArgument::OPTIONAL,
+                 'The name of the new module.'
+             )
+             ->addArgument(
+                 'namespace',
+                 InputArgument::OPTIONAL,
+                 'The class namespace of the new module.'
+             );
+    }
 
-	protected function configure() {
-		$this->setName($this->name)
-			 ->setDescription($this->description)
-			 ->addArgument(
-				 'name',
-				 InputArgument::OPTIONAL,
-				 'The name of the new module.'
-			 )
-			 ->addArgument(
-				 'namespace',
-				 InputArgument::OPTIONAL,
-				 'The class namespace of the new module.'
-			 );
-	}
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getHelper('dialog');
+        $name = $input->getArgument('name');
+        if (!$name) {
+            $name = $dialog->ask($output, 'Name of module: ');
+        }
+        $name = strtolower($name);
 
-	public function go(Console $console) {
-		$name = $this->input->getArgument('name');
-		if(!$name) {
-			$name = $console->ask('Name of module: ');
-		}
-		$name = strtolower($name);
-		$namespace = $this->input->getArgument('namespace');
-		if(!$namespace) {
-			$namespace = $console->ask('Namespace for this module: ', S::upperCamelize($name));
-		}
-		//create dirs
-		$this->createDirectories($this->createModuleDirectory($namespace));
-		//create config.php
-		$file = $this->createModuleDirectory($namespace) . 'config.php';
-		$console->verbose("Creating <info>$file</info>");
-		$config = new Config('new-module', $file);
-		$config->set('namespace', $namespace);
-		$config->set('assets.dir', 'assets/');
-		$config->save();
-		//add module to neptune modules config
-		$this->config->set('modules.' . $name, $this->createModuleDirectory($namespace, false));
-		$this->config->save();
-		//create controller
-		//$this->runCommand
-		//create view
-		//create services.php
-		//create routes.php
-		//create command
-		//update composer.json autoload
-	}
+        $namespace = $input->getArgument('namespace');
+        if (!$namespace) {
+            $namespace = $dialog->ask($output, 'Namespace for this module: ', S::upperCamelize($name));
+        }
 
-	public function isEnabled() {
-		return true;
-	}
+        $this->createDirectories($output, $this->createModuleDirectory($namespace));
 
-	protected function createModuleDirectory($namespace, $absolute = true) {
-		if(substr($namespace, -1) !== '/') {
-			$namespace .= '/';
-		}
-		if($absolute) {
-			return $this->getRootDirectory() . 'app/' . $namespace;
-		}
-		return 'app/' . $namespace;
-	}
+        $file = $this->createModuleDirectory($namespace) . 'config.php';
+        //hacky method for now until Config can create a new file.
+        file_put_contents($file, '<?php return array ();');
+        $output->writeln("Created <info>$file</info>");
 
-	protected function createDirectories($root) {
-		foreach($this->dirs as $dir) {
-			$dir = $root . $dir;
-			if(!file_exists($dir)) {
-				mkdir($dir, 0755, true);
-				$this->console->verbose(sprintf("Creating directory <info>%s</info>", $dir));
-			}
-		}
-	}
+        //create module class
+    }
+
+    protected function createModuleDirectory($namespace, $absolute = true)
+    {
+        if (substr($namespace, -1) !== '/') {
+            $namespace .= '/';
+        }
+        if ($absolute) {
+            return $this->getRootDirectory() . 'src/' . $namespace;
+        }
+
+        return 'src/' . $namespace;
+    }
+
+    protected function createDirectories(OutputInterface $output, $root)
+    {
+        $dirs = array(
+            'views',
+            'assets'
+        );
+        foreach ($dirs as $dir) {
+            $dir = $root . $dir;
+            if (file_exists($dir)) {
+                continue;
+            }
+            mkdir($dir, 0755, true);
+            if ($output->getVerbosity() === OutputInterface::VERBOSITY_VERBOSE) {
+                $output->writeln(sprintf("Creating directory <info>%s</info>", $dir));
+            }
+        }
+    }
 
 }
