c2d29077dda02e7b67fe23dec5bfe7cdf31c4991
Glynn Forrest <me@glynnforrest.com>
2014-10-28 23:34:38 +0000

    Updating create:entity command.
    Using Doctrine Inflector instead of Crutches - it is removed in Crutches
    0.3.

--- src/Neptune/Command/CreateEntityCommand.php
+++ src/Neptune/Command/CreateEntityCommand.php
@@ -2,34 +2,36 @@
 
 namespace Neptune\Command;
 
-use Neptune\Command\CreateCommand;
 use Neptune\View\Skeleton;
 
-use Crutches\Inflector;
-
 use Stringy\StaticStringy as S;
+
+use Doctrine\Common\Inflector\Inflector;
 
 /**
  * CreateEntityCommand
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class CreateEntityCommand extends CreateCommand {
-
-	protected $name = 'create:entity';
+class CreateEntityCommand extends CreateCommand
+{
+    protected $name = 'create:entity';
     protected $description = 'Create a new active doctrine entity and test';
-	protected $prompt = 'Entity name (a singular noun): ';
-	protected $default = 'User';
+    protected $prompt = 'Entity name (a singular noun): ';
+    protected $default = 'User';
 
-	protected function getTargetPath($name) {
-		return 'Entity/' . S::UpperCamelize($name) . '.php';
-	}
+    protected function getTargetPath($name)
+    {
+        return 'Entity/' . S::upperCamelize($name) . '.php';
+    }
 
-	protected function getSkeleton($name) {
+    protected function getSkeleton($name)
+    {
         $skeleton = new Skeleton($this->getSkeletonPath('entity'));
-		$name = S::UpperCamelize($name);
-		$skeleton->entity_name = $name;
-		$skeleton->table = Inflector::locale()->plural(S::slugify($name, '_'));
-		return $skeleton;
-	}
+        $name = S::upperCamelize($name);
+        $skeleton->entity_name = $name;
+        $skeleton->table = Inflector::pluralize(S::replace(S::dasherize($name), '-', '_'));
+
+        return $skeleton;
+    }
 
 }
