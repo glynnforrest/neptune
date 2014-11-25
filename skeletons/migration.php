60e9f61089b9fdf962437df6935013edd18e97c5
Glynn Forrest <me@glynnforrest.com>
2014-11-25 14:24:09 +0000

    Improving migration skeleton.

--- skeletons/migration.php
+++ skeletons/migration.php
@@ -12,12 +12,18 @@ class <?=$this->class_name;?> extends AbstractMigration
 
     public function up(Schema $schema)
     {
+        $table = $schema->createTable('');
 
+        $id = $table->addColumn('id', 'integer', ['unsigned' => true]);
+        $id->setAutoIncrement(true);
+        $table->setPrimaryKey(['id']);
+
+        return $schema;
     }
 
     public function down(Schema $schema)
     {
-
+        $schema->dropTable('');
     }
 
 }
