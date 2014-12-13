c4ea5f90cf229fb8ff5a7d37af12a96e790589b0
Glynn Forrest <me@glynnforrest.com>
2014-12-13 18:26:00 +0000

    Updating namespace in test bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-if (!$loader = @include __DIR__ . '/../vendor/autoload.php') {
-	echo 'Composer autoloader not found.' . PHP_EOL;
-	echo 'Please run `composer install`.' . PHP_EOL;
-	exit(1);
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    echo 'Composer autoloader not found.'.PHP_EOL;
+    echo 'Please run `composer install`.'.PHP_EOL;
+    exit(1);
 }
 
-$loader->add('Neptune\Test', __DIR__);
+$loader->add('Neptune', __DIR__);
