1e4767f9fc7146d5adb00b6ef00debe73401b4f3
Glynn Forrest <me@glynnforrest.com>
2014-12-16 16:24:50 +0000

    Adding SetupApplication for installer script.

---
+++ src/Neptune/Console/SetupApplication.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Neptune\Console;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Neptune\Command\SetupCommand;
+
+/**
+ * SetupApplication
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class SetupApplication extends Application
+{
+    /**
+     * Gets the name of the command based on input.
+     *
+     * @param InputInterface $input The input interface
+     *
+     * @return string The command name
+     */
+    protected function getCommandName(InputInterface $input)
+    {
+        return 'setup';
+    }
+
+    /**
+     * Gets the default commands that should always be available.
+     *
+     * @return array An array of default Command instances
+     */
+    protected function getDefaultCommands()
+    {
+        // Keep the core default commands to have the HelpCommand
+        // which is used when using the --help option
+        $defaultCommands = parent::getDefaultCommands();
+
+        $defaultCommands[] = new SetupCommand();
+
+        return $defaultCommands;
+    }
+
+    /**
+     * Overridden so that the application doesn't expect the command
+     * name to be the first argument.
+     */
+    public function getDefinition()
+    {
+        $inputDefinition = parent::getDefinition();
+        // clear out the normal first argument, which is the command name
+        $inputDefinition->setArguments();
+
+        return $inputDefinition;
+    }
+}
