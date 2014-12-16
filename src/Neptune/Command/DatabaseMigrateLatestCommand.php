9c440150269898d039dbbf2294973309f861a920
Glynn Forrest <me@glynnforrest.com>
2014-12-16 17:32:29 +0000

    database:migration:latest uses getModuleArgument().

--- src/Neptune/Command/DatabaseMigrateLatestCommand.php
+++ src/Neptune/Command/DatabaseMigrateLatestCommand.php
@@ -4,10 +4,9 @@ namespace Neptune\Command;
 
 use Neptune\Console\ConsoleLogger;
 use Neptune\Database\Migration\MigrationRunner;
-
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 
 /**
  * DatabaseMigrateLatestCommand
@@ -16,27 +15,24 @@ use Symfony\Component\Console\Input\InputOption;
  **/
 class DatabaseMigrateLatestCommand extends Command
 {
-
     protected $name = 'database:migrate:latest';
     protected $description = 'Migrate to the latest version of the database schema';
 
     protected function configure()
     {
         parent::configure();
-        $this->addOption(
+        $this->addArgument(
                  'module',
-                 'm',
-                 InputOption::VALUE_REQUIRED,
-                 'The module containing the migrations',
-                 $this->getDefaultModule()
+                 InputArgument::OPTIONAL,
+                 'The module containing the migrations'
              );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $runner = new MigrationRunner($this->neptune['db'], new ConsoleLogger($output));
-        $module = $input->getOption('module');
-        $runner->migrateLatest($this->neptune->getModule($module));
+        $module = $this->getModuleArgument($input, $output);
+        $runner->migrateLatest($module);
+        $output->writeln(sprintf('Migrated <info>%s</info> to the latest version.', $module->getName()));
     }
-
 }
