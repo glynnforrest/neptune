ba2f839ece7d46b7e5e57db264f5da43adb45667
Glynn Forrest <me@glynnforrest.com>
2014-12-30 15:30:55 +0000

    Updating shell command.

--- src/Neptune/Command/ShellCommand.php
+++ src/Neptune/Command/ShellCommand.php
@@ -2,29 +2,26 @@
 
 namespace Neptune\Command;
 
-use Neptune\Command\Command;
-use Neptune\Console\Console;
 use Neptune\Console\Shell;
-
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
 
 /**
  * ShellCommand
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class ShellCommand extends Command {
+class ShellCommand extends Command
+{
+    protected $name = 'shell';
+    protected $description = 'Run commands in a shell';
 
-	protected $name = 'shell';
-	protected $description = 'Run commands in a shell';
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $app = $this->getApplication();
+        $shell = new Shell($app);
+        $shell->run();
 
-	public function go(Console $console) {
-		$app = $this->getApplication();
-		$shell = new Shell($app);
-		$shell->run();
-		return 0;
-	}
-
+        return 0;
+    }
 }
