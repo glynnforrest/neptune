18e5e99a8e60691235bba4c2e0c9283636bd90cd
Glynn Forrest <me@glynnforrest.com>
2015-01-11 14:47:27 +0000

    Refactoring SecurityService to lazy load config.

--- src/Neptune/Service/SecurityService.php
+++ src/Neptune/Service/SecurityService.php
@@ -6,13 +6,9 @@ use Neptune\Core\Neptune;
 use Neptune\Config\Config;
 use Neptune\Security\SecurityFactory;
 use Neptune\Security\SecurityRequestListener;
-
 use Blockade\Firewall;
-use Blockade\CsrfManager;
 use Blockade\EventListener\FirewallListener;
 use Blockade\EventListener\BlockadeExceptionListener;
-
-use Reform\EventListener\CsrfListener;
 use Symfony\Component\HttpFoundation\RequestMatcher;
 
 /**
@@ -22,32 +18,22 @@ use Symfony\Component\HttpFoundation\RequestMatcher;
  **/
 class SecurityService implements ServiceInterface
 {
-
-    protected $config;
-
-    public function __construct(Config $config = null)
-    {
-        $this->config = $config;
-    }
-
     public function register(Neptune $neptune)
     {
-        //if no config was supplied, grab the default
-        $config = $this->config ? $this->config : $neptune['config'];
-
-        $neptune['security'] = function () use ($neptune, $config) {
-            return new SecurityFactory($config, $neptune);
+        $neptune['security'] = function ($neptune) {
+            return new SecurityFactory($neptune['config'], $neptune);
         };
 
-        //register firewalls if defined
-        if ($config->get('security.firewalls', false)) {
-            $this->registerFirewall($neptune, $config);
-        }
+        $neptune['security.firewall'] = function ($neptune) {
+            $listener = new FirewallListener();
+            $config = $neptune['config'];
 
-        //register csrf by default
-        if ($config->get('security.csrf', true)) {
-            $this->registerCsrf($neptune, $config);
-        }
+            foreach ($config->get('security.firewalls', []) as $name => $firewall) {
+                $listener->addFirewall($this->createFirewall($neptune, $config, $name));
+            }
+
+            return $listener;
+        };
 
         $neptune['security.resolver'] = function () {
             return new BlockadeExceptionListener();
@@ -56,18 +42,6 @@ class SecurityService implements ServiceInterface
 
         $neptune['security.request'] = function ($neptune) {
             return new SecurityRequestListener($neptune['security']);
-        };
-    }
-
-    protected function registerFirewall(Neptune $neptune, Config $config)
-    {
-        $neptune['security.firewall'] = function () use ($neptune, $config) {
-            $listener = new FirewallListener();
-            foreach ($config->get('security.firewalls', array()) as $name => $firewall) {
-                $listener->addFirewall($this->createFirewall($neptune, $config, $name));
-            }
-
-            return $listener;
         };
     }
 
@@ -92,35 +66,12 @@ class SecurityService implements ServiceInterface
         return $firewall;
     }
 
-    protected function registerCsrf(Neptune $neptune, Config $config)
-    {
-        $neptune['security.csrf'] = function () use ($neptune, $config) {
-            //get the session and form tokens
-            $session_token = $config->get('security.csrf.session_token', 'security.csrf.token');
-            $form_token = $config->get('security.csrf.form_token', '_token');
-
-            $manager = new CsrfManager($neptune['session'], $session_token);
-
-            return new CsrfListener($manager, $form_token);
-        };
-    }
-
     public function boot(Neptune $neptune)
     {
         $dispatcher = $neptune['dispatcher'];
 
         $dispatcher->addSubscriber($neptune['security.resolver']);
         $dispatcher->addSubscriber($neptune['security.request']);
-
-        //add the firewall if set up
-        if ($neptune->offsetExists('security.firewall')) {
-            $neptune['dispatcher']->addSubscriber($neptune['security.firewall']);
-        }
-
-        //add csrf if set up
-        if ($neptune->offsetExists('security.csrf')) {
-            $neptune['dispatcher']->addSubscriber($neptune['security.csrf']);
-        }
+        $dispatcher->addSubscriber($neptune['security.firewall']);
     }
-
 }
