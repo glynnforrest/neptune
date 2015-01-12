08407a9e88e7b9bf5a9d8ec62f83cbb36e45868c
Glynn Forrest <me@glynnforrest.com>
2015-01-12 18:13:34 +0000

    Refactoring ServiceListCommand to show nested containers.

--- src/Neptune/Command/ServiceListCommand.php
+++ src/Neptune/Command/ServiceListCommand.php
@@ -5,10 +5,10 @@ namespace Neptune\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Pimple\Container;
 
 class ServiceListCommand extends Command
 {
-
     protected $name = 'service:list';
     protected $description = 'List all available services';
 
@@ -26,37 +26,65 @@ class ServiceListCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $services = $this->neptune->keys();
-
         $output->writeln('<info>Available services</info>');
 
-        sort($services);
-
         if ($input->getOption('names')) {
-            foreach ($services as $service) {
+            foreach ($this->neptune->keys() as $service) {
                 $output->writeln($service);
             }
-        } else {
-            foreach ($services as $service) {
-                try {
-                    $output->writeln(sprintf('%s: %s', $service, $this->asString($this->neptune[$service])));
-                } catch (\Exception $e) {
-                    throw new \Exception(sprintf('An exception occurred resolving "%s": %s', $service, $e->getMessage()));
-                }
+
+            return;
+        }
+
+        $this->printTree($output, $this->buildTree($this->neptune));
+    }
+
+    protected function printTree(OutputInterface $output, array $tree, $depth = 0)
+    {
+        foreach ($tree as $service => $description) {
+            $depth_prefix = str_repeat('    ', $depth);
+
+            if (is_array($description)) {
+                $output->writeln($depth_prefix.$service.': Pimple\Container');
+                $this->printTree($output, $description, $depth + 1);
+                continue;
             }
+
+            $output->writeln($depth_prefix.$service.': '.$description);
         }
     }
 
-    public function asString($object)
+    protected function buildTree(Container $container)
+    {
+        $services = $container->keys();
+        sort($services);
+        $tree = [];
+
+        foreach ($services as $service) {
+            try {
+                $tree[$service] = $this->describeObject($container[$service]);
+            } catch (\Exception $e) {
+                throw new \Exception(sprintf('An exception occurred resolving "%s": %s', $service, $e->getMessage()));
+            }
+        }
+
+        return $tree;
+    }
+
+    protected function describeObject($object)
     {
         if (is_object($object)) {
+            if ($object instanceof Container) {
+                return $this->buildTree($object);
+            }
+
             return get_class($object);
         }
+
         if (is_array($object)) {
             return sprintf('(array with %s items)', count($object));
         }
 
         return var_export($object, true);
     }
-
 }
