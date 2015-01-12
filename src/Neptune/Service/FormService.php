7c45e8d17d672d8c49187fe036dc8ef304e0e753
Glynn Forrest <me@glynnforrest.com>
2015-01-12 18:08:05 +0000

    Moving csrf services to FormService.

--- src/Neptune/Service/FormService.php
+++ src/Neptune/Service/FormService.php
@@ -4,6 +4,8 @@ namespace Neptune\Service;
 
 use Neptune\Core\Neptune;
 use Neptune\Form\FormCreator;
+use Reform\Csrf\CsrfChecker;
+use Reform\EventListener\CsrfListener;
 
 /**
  * FormService
@@ -12,16 +14,31 @@ use Neptune\Form\FormCreator;
  **/
 class FormService implements ServiceInterface
 {
-
     public function register(Neptune $neptune)
     {
         $neptune['form'] = function ($neptune) {
+            //register csrf by default
+            if ($neptune['config']->get('security.csrf', true)) {
+                $neptune['dispatcher']->addSubscriber($neptune['security.csrf']);
+            }
+
             return new FormCreator($neptune, $neptune['dispatcher']);
+        };
+
+        $neptune['security.csrf'] = function ($neptune) {
+            $config = $neptune['config'];
+
+            //get the session and form tokens
+            $session_token = $config->get('security.csrf.session_token', 'security.csrf.token');
+            $form_token = $config->get('security.csrf.form_token', '_token');
+
+            $manager = new CsrfChecker($neptune['session'], $session_token);
+
+            return new CsrfListener($manager, $form_token, true);
         };
     }
 
     public function boot(Neptune $neptune)
     {
     }
-
 }
