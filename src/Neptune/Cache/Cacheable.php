2de889ab64a8c30f41a755058e94a876385aeaee
Glynn Forrest <me@glynnforrest.com>
2015-01-22 17:07:44 +0000

    Simplifying conditionals and cleanup code style in Cacheable.

--- src/Neptune/Cache/Cacheable.php
+++ src/Neptune/Cache/Cacheable.php
@@ -9,52 +9,51 @@ use Doctrine\Common\Cache\Cache;
  * Cacheable
  * @author Glynn Forrest <me@glynnforrest.com>
  */
-abstract class Cacheable {
+abstract class Cacheable
+{
+    private $cache;
 
-	private $cache;
+    public function __call($method, $args)
+    {
+        if (substr($method, -6) !== 'Cached' || method_exists($this, $method)) {
+            throw new MethodNotFoundException('Function not found: '.$method);
+        }
+        $method = substr($method, 0, -6);
 
-	public function __call($method, $args) {
-		if (substr($method, -6) === 'Cached') {
-			return $this->getCachedResult(substr($method, 0, -6), $args);
-		} else {
-			throw new MethodNotFoundException('Function not found: ' . $method);
-		}
-	}
+        //if there is no cache available, just call the method
+        if (!$this->cache) {
+            return call_user_func_array(array($this, $method), $args);
+        }
 
-	protected function getCachedResult($method, $args) {
-		if (method_exists($this, $method)) {
-			//if there is no cache available, just call the method
-			if(!$this->cache) {
-				return call_user_func_array(array($this, $method), $args);
-			}
-			//build key
-			$key = get_class($this) . ':' . $method;
-			//create a unique hash of the args
-			foreach($args as $arg) {
-				$key .= ':' . serialize($arg);
-			}
-			//make sure the key isn't too long
-			$key = md5($key);
-			//check for a cached version. if it does, return the value
-			$result = $this->cache->fetch($key);
-			if(false !== $result) {
-				return $result;
-			}
-			//doesn't, lets call the function and cache it
-			$result = call_user_func_array(array($this, $method), $args);
-			$this->cache->save($key, $result);
-			return $result;
-		} else {
-			throw new MethodNotFoundException('Function not found: ' . $method);
-		}
-	}
+        //build key
+        $key = get_class($this).':'.$method;
+        //create a unique hash of the args
+        foreach ($args as $arg) {
+            $key .= ':'.serialize($arg);
+        }
+        //make sure the key isn't too long
+        $key = md5($key);
 
-	public function setCache(Cache $cache) {
-		$this->cache = $cache;
-	}
+        //check for a cached version. if it does, return the value
+        $result = $this->cache->fetch($key);
+        if (false !== $result) {
+            return $result;
+        }
 
-	public function getCache() {
-		return $this->cache;
-	}
+        //doesn't, call the function and cache it
+        $result = call_user_func_array(array($this, $method), $args);
+        $this->cache->save($key, $result);
 
+        return $result;
+    }
+
+    public function setCache(Cache $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    public function getCache()
+    {
+        return $this->cache;
+    }
 }
