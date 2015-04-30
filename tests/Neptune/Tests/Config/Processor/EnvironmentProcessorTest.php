7aea8567b48422f620538fd1cd9bbd6b6007aaf6
Glynn Forrest <me@glynnforrest.com>
2015-04-30 18:45:10 +0100

    EnvironmentProcessor runs on preMerge.
    This allows for the added variables to be used in references.

--- tests/Neptune/Tests/Config/Processor/EnvironmentProcessorTest.php
+++ tests/Neptune/Tests/Config/Processor/EnvironmentProcessorTest.php
@@ -4,9 +4,11 @@ namespace Neptune\Tests\Config\Processor;
 
 use Neptune\Config\Processor\EnvironmentProcessor;
 use Neptune\Config\Config;
+use Neptune\Config\ConfigManager;
+use Neptune\Config\Processor\ReferenceProcessor;
 
 /**
- * EnvironmentProcessorTest
+ * EnvironmentProcessorTest.
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
@@ -29,10 +31,27 @@ class EnvironmentProcessorTest extends \PHPUnit_Framework_TestCase
         $this->neptune->expects($this->once())
             ->method('getEnv')
             ->will($this->returnValue('dev'));
-        $this->processor->onPostMerge($config);
+        $this->processor->onPreMerge($config, []);
         $this->assertSame([
             'ROOT' => '/path/to/app',
             'ENV' => 'dev',
         ], $config->get());
+    }
+
+    public function testVariablesCanBeUsedForReferences()
+    {
+        $manager = new ConfigManager();
+        $manager->loadValues([
+            'log_path' => '%ROOT%/logs/%ENV%.log',
+        ]);
+        $manager->addProcessor(new ReferenceProcessor());
+        $manager->addProcessor($this->processor);
+        $this->neptune->expects($this->once())
+            ->method('getRootDirectory')
+            ->will($this->returnValue('/path/to/app'));
+        $this->neptune->expects($this->once())
+            ->method('getEnv')
+            ->will($this->returnValue('dev'));
+        $this->assertSame('/path/to/app/logs/dev.log', $manager->getConfig()->get('log_path'));
     }
 }
