9123074c4a6317ddcc1c64abb969f3b7cd6f77b7
Glynn Forrest <me@glynnforrest.com>
2015-11-05 13:38:05 +0000

    Add --exclude-modules option to database:fixtures:run

--- src/Neptune/Command/DatabaseFixturesRunCommand.php
+++ src/Neptune/Command/DatabaseFixturesRunCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Neptune\Service\AbstractModule;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Neptune\Database\FixtureLoader;
-use Neptune\Command\Command;
 
 /**
  * DatabaseFixturesRunCommand.
@@ -26,8 +25,13 @@ class DatabaseFixturesRunCommand extends Command
         $this->addOption(
             'modules',
             'm',
-            InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL,
+            InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
             'Only run fixtures for the given modules'
+        )->addOption(
+            'exclude-modules',
+            'x',
+            InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
+            'Exclude fixtures in the given modules'
         )->addOption(
             'append',
             '',
@@ -42,14 +46,8 @@ class DatabaseFixturesRunCommand extends Command
         $loader = new FixtureLoader($this->neptune);
         $loader->setLogger(new ConsoleLogger($output));
 
-        $modules = $input->getOption('modules') ?
-                 array_map(function($moduleName) {
-                     return $this->neptune->getModule($moduleName);
-                 }, $input->getOption('modules')) :
-                 $this->neptune->getModules();
-
         $fixtures = [];
-        foreach ($modules as $module) {
+        foreach ($this->getInputModules($input) as $module) {
             $fixtures = array_merge($fixtures, $this->getModuleFixtures($module));
         }
 
@@ -87,5 +85,31 @@ class DatabaseFixturesRunCommand extends Command
         }
 
         return $fixtures;
+    }
+
+    /**
+     * Get modules from the given input.
+     * --modules takes priority over --exclude-modules.
+     * Defaults to all modules.
+     *
+     * @param InputInterface $input
+     */
+    protected function getInputModules(InputInterface $input)
+    {
+        if ($input->getOption('modules')) {
+            return array_map(function ($moduleName) {
+                return $this->neptune->getModule($moduleName);
+            }, $input->getOption('modules'));
+        }
+
+        if ($input->getOption('exclude-modules')) {
+            $excludedModules = $input->getOption('exclude-modules');
+
+            return array_filter($this->neptune->getModules(), function ($module) use ($excludedModules) {
+                return !in_array($module->getName(), $excludedModules);
+            });
+        }
+
+        return $this->neptune->getModules();
     }
 }
